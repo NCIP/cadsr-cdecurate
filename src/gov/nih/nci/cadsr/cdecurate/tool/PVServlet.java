41653536abb26f08091fb701856bc2428a0f89d7
chickerura <chickerura@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2008-03-13 20:34:43 +0000

    Changes to the code for the requirement VM as AC. changes needed to display the vm edit form
    SVN-Revision: 477

--- src/gov/nih/nci/cadsr/cdecurate/tool/PVServlet.java
+++ src/gov/nih/nci/cadsr/cdecurate/tool/PVServlet.java
@@ -1,6 +1,6 @@
 // Copyright ScenPro, Inc 2007
 
-// $Header: /cvsshare/content/cvsroot/cdecurate/src/gov/nih/nci/cadsr/cdecurate/tool/PVServlet.java,v 1.27 2008-02-20 19:35:06 chickerura Exp $
+// $Header: /cvsshare/content/cvsroot/cdecurate/src/gov/nih/nci/cadsr/cdecurate/tool/PVServlet.java,v 1.28 2008-03-13 20:34:43 chickerura Exp $
 // $Name: not supported by cvs2svn $
 
 package gov.nih.nci.cadsr.cdecurate.tool;
@@ -888,7 +888,7 @@ public class PVServlet implements Serializable
          DataManager.setAttribute(session, VMForm.SESSION_SELECT_PV, selPV);
          DataManager.setAttribute(session, PVForm.SESSION_PV_INDEX, pvInd);
          DataManager.setAttribute(session, VMForm.SESSION_VM_TAB_FOCUS, VMForm.ELM_ACT_DETAIL_TAB);
-
+         DataManager.setAttribute(session, VMForm.SESSION_RET_PAGE, VMForm.ACT_BACK_PV);
          //get vm alt names
          AltNamesDefsServlet altSer = new AltNamesDefsServlet(data.getCurationServlet(), data.getCurationServlet().sessionData.UsrBean);
          Alternates alt = altSer.getManualDefinition(data.getRequest(), VMForm.ELM_FORM_SEARCH_EVS);
