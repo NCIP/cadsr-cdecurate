e9faf260a80a2939dc6e72fedca1e1de85d6d13e
hegdes <hegdes@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2008-10-17 18:34:52 +0000

    initial version of persist layer
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@760 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

---
+++ src/gov/nih/nci/cadsr/persist/common/DBConstants.java
@@ -0,0 +1,18 @@
+package gov.nih.nci.cadsr.persist.common;
+
+public class DBConstants {
+	
+	public static final String RECORD_DELETED_YES = "Yes";
+	public static final String RECORD_DELETED_NO = "No";
+    
+	public static final String LATEST_VERSION_IND_YES = "Yes";
+	public static final String LATEST_VERSION_IND_NO ="No";
+    
+	public static final String DEFALULT_ASL_INSERT = "DRAFT NEW";
+	public static final String ASL_NAME_RELEASED  = "RELEASED";
+	
+	public static final String ASTL_NAME_DATA_ELEMENT = "DATAELEMENT";
+	
+	public static final String AL_NAME_VERSIONED = "VERSIONED";
+
+}
