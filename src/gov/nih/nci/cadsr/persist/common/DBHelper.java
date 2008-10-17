0242ebdb8acb580da859ac87b85a3fd2cab70ea8
hegdes <hegdes@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2008-10-17 18:34:52 +0000

    initial version of persist layer
    SVN-Revision: 760

---
+++ src/gov/nih/nci/cadsr/persist/common/DBHelper.java
@@ -0,0 +1,45 @@
+package gov.nih.nci.cadsr.persist.common;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.apache.log4j.*;
+
+public class DBHelper {
+
+	private static Logger logger = Logger.getLogger(DBHelper.class);
+	
+	public DBHelper(){
+	}
+
+	/**This method closes the resultset and statement object
+	 * 
+	 * @param rs
+	 * @param stmt
+	 */
+	public static void close(ResultSet rs, Statement stmt) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				logger.warn("Failed to close resultset", e);
+			}
+		}
+		close(stmt);
+	}
+	
+	/**This method closes the statement object
+	 * 
+	 * @param stmt
+	 */
+	public static void close(Statement stmt) {
+		if (stmt != null) {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				logger.warn("Error closing statement");
+			}
+		}
+	}
+
+}
