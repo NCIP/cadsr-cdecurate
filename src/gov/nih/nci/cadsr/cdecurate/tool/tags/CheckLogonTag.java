39960f29bba06d9424ec3693ff78a5c19f8b00ab
veerlah <veerlah@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2008-11-20 17:04:07 +0000

    Moved this tag from tools package to tags package(created separate package for tags)
    SVN-Revision: 789

---
+++ src/gov/nih/nci/cadsr/cdecurate/tool/tags/CheckLogonTag.java
@@ -0,0 +1,184 @@
+// Copyright (c) 2002 ScenPro, Inc.
+
+// $Header: /cvsshare/content/cvsroot/cdecurate/src/gov/nih/nci/cadsr/cdecurate/tool/tags/CheckLogonTag.java,v 1.1 2008-11-20 17:04:07 veerlah Exp $
+// $Name: not supported by cvs2svn $
+
+package gov.nih.nci.cadsr.cdecurate.tool.tags;
+
+import javax.servlet.jsp.tagext.*;
+import javax.servlet.jsp.JspException;
+import javax.servlet.http.HttpSession;
+
+/**
+ * The CheckLogonTag class is a JSP tag library class to provide a JSP tag to
+ * check the session to verify that the user is logged on.  This will look for
+ * the UserBean in the session and if it is not there redirect them to the
+ * logon page.
+ * <P>
+ * @author Michael Holck
+ * @version 3.0
+ */
+
+ /*
+The CaCORE Software License, Version 3.0 Copyright 2002-2005 ScenPro, Inc. (“ScenPro”)  
+Copyright Notice.  The software subject to this notice and license includes both
+human readable source code form and machine readable, binary, object code form
+(“the CaCORE Software”).  The CaCORE Software was developed in conjunction with
+the National Cancer Institute (“NCI”) by NCI employees and employees of SCENPRO.
+To the extent government employees are authors, any rights in such works shall
+be subject to Title 17 of the United States Code, section 105.    
+This CaCORE Software License (the “License”) is between NCI and You.  “You (or “Your”)
+shall mean a person or an entity, and all other entities that control, are 
+controlled by, or are under common control with the entity.  “Control” for purposes
+of this definition means (i) the direct or indirect power to cause the direction
+or management of such entity, whether by contract or otherwise, or (ii) ownership
+of fifty percent (50%) or more of the outstanding shares, or (iii) beneficial 
+ownership of such entity.  
+This License is granted provided that You agree to the conditions described below.
+NCI grants You a non-exclusive, worldwide, perpetual, fully-paid-up, no-charge,
+irrevocable, transferable and royalty-free right and license in its rights in the
+CaCORE Software to (i) use, install, access, operate, execute, copy, modify, 
+translate, market, publicly display, publicly perform, and prepare derivative 
+works of the CaCORE Software; (ii) distribute and have distributed to and by 
+third parties the CaCORE Software and any modifications and derivative works 
+thereof; and (iii) sublicense the foregoing rights set out in (i) and (ii) to 
+third parties, including the right to license such rights to further third parties.
+For sake of clarity, and not by way of limitation, NCI shall have no right of 
+accounting or right of payment from You or Your sublicensees for the rights 
+granted under this License.  This License is granted at no charge to You.
+1.	Your redistributions of the source code for the Software must retain the above
+copyright notice, this list of conditions and the disclaimer and limitation of
+liability of Article 6, below.  Your redistributions in object code form must
+reproduce the above copyright notice, this list of conditions and the disclaimer
+of Article 6 in the documentation and/or other materials provided with the 
+distribution, if any.
+2.	Your end-user documentation included with the redistribution, if any, must 
+include the following acknowledgment: “This product includes software developed 
+by SCENPRO and the National Cancer Institute.”  If You do not include such end-user
+documentation, You shall include this acknowledgment in the Software itself, 
+wherever such third-party acknowledgments normally appear.
+3.	You may not use the names "The National Cancer Institute", "NCI" “ScenPro, Inc.”
+and "SCENPRO" to endorse or promote products derived from this Software.  
+This License does not authorize You to use any trademarks, service marks, trade names,
+logos or product names of either NCI or SCENPRO, except as required to comply with
+the terms of this License.
+4.	For sake of clarity, and not by way of limitation, You may incorporate this
+Software into Your proprietary programs and into any third party proprietary 
+programs.  However, if You incorporate the Software into third party proprietary
+programs, You agree that You are solely responsible for obtaining any permission
+from such third parties required to incorporate the Software into such third party
+proprietary programs and for informing Your sublicensees, including without 
+limitation Your end-users, of their obligation to secure any required permissions
+from such third parties before incorporating the Software into such third party
+proprietary software programs.  In the event that You fail to obtain such permissions,
+You agree to indemnify NCI for any claims against NCI by such third parties, 
+except to the extent prohibited by law, resulting from Your failure to obtain
+such permissions.
+5.	For sake of clarity, and not by way of limitation, You may add Your own 
+copyright statement to Your modifications and to the derivative works, and You 
+may provide additional or different license terms and conditions in Your sublicenses
+of modifications of the Software, or any derivative works of the Software as a 
+whole, provided Your use, reproduction, and distribution of the Work otherwise 
+complies with the conditions stated in this License.
+6.	THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+(INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, 
+NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE) ARE DISCLAIMED.  
+IN NO EVENT SHALL THE NATIONAL CANCER INSTITUTE, SCENPRO, OR THEIR AFFILIATES 
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+public final class CheckLogonTag extends TagSupport
+{
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -5073650521238130249L;
+  // Attributes
+  private String m_name;
+  private String m_page;
+
+  /**
+   * The setName method is used to set the name of the bean in the session
+   * for us to check for.
+   *
+   * @param name The name of the UserBean in the session
+   */
+  public void setName(String name)
+  {
+    m_name = name;
+  }
+
+  /**
+   * The getName method is used to get the name of the bean in the session
+   * for us to check for.
+   *
+   * @return String The name of the UserBean in the session
+   */
+  public String getName()
+  {
+    return m_name;
+  }
+
+  /**
+   * The setPage method is used to set the page we should forward to if they
+   * are not logged in.
+   *
+   * @param page The page we should forward to
+   */
+  public void setPage(String page)
+  {
+    m_page = page;
+  }
+
+  /**
+   * The getPage method is used to get the page we should forward to if they
+   * are not logged in.
+   *
+   * @return String The page we should forward to
+   */
+  public String getPage()
+  {
+    return m_page;
+  }
+
+  /**
+   * doEndTag
+   * @return int
+   * @exception javax.servlet.jsp.JspException
+   */
+  public int doEndTag() throws JspException
+  {
+    boolean valid = false;
+    HttpSession session = pageContext.getSession();
+    int result;
+    // If the session is established and there is a UserBean then we are OK
+    if ((session != null) && (session.getAttribute(m_name) != null))
+      valid = true;
+
+    // Now set the result based on valid
+    if (valid)
+      result = EVAL_PAGE;
+    else
+    {
+      // Forward them to the logon page
+      try
+      {
+        pageContext.forward(m_page);
+      }
+      catch(Exception e)
+      {
+        throw new JspException(e.toString());
+      }
+      
+      result = SKIP_PAGE;
+    }
+      
+    return result;
+  }
+}
+
+ 
