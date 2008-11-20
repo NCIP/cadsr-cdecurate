42b976b6c2602f1eed7d92fac99e7736c71d6d50
veerlah <veerlah@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2008-11-20 20:23:25 +0000

    added code to  create new DE,DEC,VD
    SVN-Revision: 806

--- src/gov/nih/nci/cadsr/cdecurate/tool/tags/CreateMenuTag.java
+++ src/gov/nih/nci/cadsr/cdecurate/tool/tags/CreateMenuTag.java
@@ -2,30 +2,45 @@ package gov.nih.nci.cadsr.cdecurate.tool.tags;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpSession;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 
+import org.apache.commons.lang.StringEscapeUtils;
+
+/**
+ * This JSP tag library class is for Create Menu
+ * @author hveerla
+ *
+ */
 public class CreateMenuTag extends MenuTag {
 	public int doEndTag() throws JspException {
 		JspWriter createMenu = this.pageContext.getOut();
-		
+		HttpSession session = pageContext.getSession();
+		String userName = null;
+		if (session != null) {
+		   userName = (String) session.getAttribute("Username");
+		}
 		try {
+			createMenu.println("<form name=\"newDEForm\" method=\"post\" action=\"../../cdecurate/NCICurationServlet?reqType=newDEFromMenu\"></form>"
+		                       + "<form name=\"newDECForm\" method=\"post\" action=\"../../cdecurate/NCICurationServlet?reqType=newDECFromMenu\"></form>"
+		                       + "<form name=\"newVDForm\" method=\"post\" action=\"../../cdecurate/NCICurationServlet?reqType=newVDFromMenu\"></form>");
 			createMenu.println("<div class=\"popMenu\">"
 					            +"<b>Data Element</b>"
 					            +"<dl class=\"menu2\">"
-					            +generateDT("New")
+					            +generateDT("","javascript:callDENew('"+StringEscapeUtils.escapeJavaScript(userName)+"')","New")
 					            +generateDT("New Using Existing")
 					            +generateDT("New Version")
 					            +"</dl>"
 					            +"<b>Data Element Concept</b>"
 					            +"<dl class=\"menu2\">"
-					            +generateDT("New")
+					            +generateDT("","javascript:callDECNew('"+StringEscapeUtils.escapeJavaScript(userName)+"')","New")
 					            +generateDT("New Using Existing")
 					            +generateDT("New Version")
 					            +"</dl>"
 					            +"<b>Value Domain</b>"
 					            +"<dl class=\"menu2\">"
-					            +generateDT("New")
+					            +generateDT("","javascript:callVDNew('"+StringEscapeUtils.escapeJavaScript(userName)+"')","New")
 					            +generateDT("New Using Existing")
 					            +generateDT("New Version")
 					            +"</dl></div>");
