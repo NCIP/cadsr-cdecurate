9c88c07851be7878c29737264550a1d0b21c39da
veerlah <veerlah@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2008-11-24 19:26:50 +0000

    changed absolute references to relative references
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@814 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

--- src/gov/nih/nci/cadsr/cdecurate/tool/tags/FooterTag.java
+++ src/gov/nih/nci/cadsr/cdecurate/tool/tags/FooterTag.java
@@ -2,6 +2,7 @@ package gov.nih.nci.cadsr.cdecurate.tool.tags;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.TagSupport;
@@ -14,6 +15,7 @@ public class FooterTag extends TagSupport {
 	private static final long serialVersionUID = 2566181324552741768L;
 
 	public int doEndTag() throws JspException {
+		HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
 		JspWriter NCIFooter = this.pageContext.getOut();
 		try {
 			NCIFooter.println("<div class=\"xyz\">"
@@ -56,10 +58,10 @@ public class FooterTag extends TagSupport {
 							+ "</table>"
 							+"<div class=\"footerBanner2\">"
 			                +"<a href=\"http://www.cancer.gov/\""
-			                 +"target=\"_blank\"><img src=\"http://ncicb.nci.nih.gov/NCICB/images/footer_nci.gif\"  width=\"63\" height=\"31\" alt=\"National Cancer Institute\"  border=\"0\"/></a>"
-			                 +"<a href=\"http://www.dhhs.gov/\"  target=\"_blank\"><img src=\"http://ncicb.nci.nih.gov/NCICB/images/footer_hhs.gif\"  width=\"39\" height=\"31\" alt=\"Department of Health and Human Services\"  border=\"0\"/></a>"
-			                +"<a href=\"http://www.nih.gov/\" target=\"_blank\"><img src=\"http://ncicb.nci.nih.gov/NCICB/images/footer_nih.gif\"   width=\"46\" height=\"31\" alt=\"National Institutes of Health\"  border=\"0\"/></a>"
-			                +"<a href=\"http://www.firstgov.gov/\"  target=\"_blank\"><img  src=\"http://ncicb.nci.nih.gov/NCICB/images/footer_firstgov.gif\" width=\"91\" height=\"31\" alt=\"FirstGov.gov\" border=\"0\"/></a>"
+			                 +"target=\"_blank\"><img src=\""+ request.getContextPath() +"/images/footer_nci.gif\"  width=\"63\" height=\"31\" alt=\"National Cancer Institute\"  border=\"0\"/></a>"
+			                 +"<a href=\"http://www.dhhs.gov/\"  target=\"_blank\"><img src=\""+ request.getContextPath() +"/images/footer_hhs.gif\"  width=\"39\" height=\"31\" alt=\"Department of Health and Human Services\"  border=\"0\"/></a>"
+			                +"<a href=\"http://www.nih.gov/\" target=\"_blank\"><img src=\""+ request.getContextPath() +"/images/footer_nih.gif\"   width=\"46\" height=\"31\" alt=\"National Institutes of Health\"  border=\"0\"/></a>"
+			                +"<a href=\"http://www.firstgov.gov/\"  target=\"_blank\"><img  src=\""+ request.getContextPath() +"/images/footer_firstgov.gif\" width=\"91\" height=\"31\" alt=\"FirstGov.gov\" border=\"0\"/></a>"
 			                +"</div></div>");
 		} catch (IOException e) {
 			e.printStackTrace();
