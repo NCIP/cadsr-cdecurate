1b2090e48aaaf3a31e2e78a2b249d51e4ca26735
veerlah <veerlah@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2009-03-31 16:55:04 +0000

    Display Datatype Scheme Reference and Datatype Annotatoin in the Value Domain create interface (#19945)
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@1240 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

---
+++ src/gov/nih/nci/cadsr/persist/ac/DataTypeVO.java
@@ -0,0 +1,85 @@
+package gov.nih.nci.cadsr.persist.ac;
+
+public class DataTypeVO {
+	private String dtl_name;
+	private String description;
+	private String comments;
+	private String scheme_reference;
+	private String annotation;
+	
+	public DataTypeVO(){
+		super();
+	}
+
+	/**
+	 * @return the dtl_name
+	 */
+	public String getDtl_name() {
+		return dtl_name;
+	}
+
+	/**
+	 * @param dtl_name the dtl_name to set
+	 */
+	public void setDtl_name(String dtl_name) {
+		this.dtl_name = dtl_name;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the comments
+	 */
+	public String getComments() {
+		return comments;
+	}
+
+	/**
+	 * @param comments the comments to set
+	 */
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	/**
+	 * @return the scheme_reference
+	 */
+	public String getScheme_reference() {
+		return scheme_reference;
+	}
+
+	/**
+	 * @param scheme_reference the scheme_reference to set
+	 */
+	public void setScheme_reference(String scheme_reference) {
+		this.scheme_reference = scheme_reference;
+	}
+
+	/**
+	 * @return the annotation
+	 */
+	public String getAnnotation() {
+		return annotation;
+	}
+
+	/**
+	 * @param annotation the annotation to set
+	 */
+	public void setAnnotation(String annotation) {
+		this.annotation = annotation;
+	}
+	
+
+}
