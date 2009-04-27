ab2a71c5c99af6e2266ac30f003ae326cae72319
hebell <hebell@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2009-04-27 19:46:23 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@1408 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

--- src/gov/nih/nci/cadsr/cdecurate/tool/InsACService.java
+++ src/gov/nih/nci/cadsr/cdecurate/tool/InsACService.java
@@ -1,4 +1,4 @@
-// $Header: /cvsshare/content/cvsroot/cdecurate/src/gov/nih/nci/cadsr/cdecurate/tool/InsACService.java,v 1.72 2009-04-23 14:14:03 veerlah Exp $
+// $Header: /cvsshare/content/cvsroot/cdecurate/src/gov/nih/nci/cadsr/cdecurate/tool/InsACService.java,v 1.73 2009-04-27 19:46:23 hebell Exp $
 // $Name: not supported by cvs2svn $
 
 package gov.nih.nci.cadsr.cdecurate.tool;
@@ -3031,6 +3031,13 @@ public class InsACService implements Serializable {
 				// PreparedStatement class)
 				// Get the username from the session.
 				String userName = (String) session.getAttribute("Username");
+				logger.info("Arguments to sbrext_set_row.set_complex_de()\nuserName\t" + userName
+				    + "\nsAction\t" + sAction
+				    + "\nsP_DE_IDSEQ\t" + sP_DE_IDSEQ
+				    + "\nsDDEMethod\t" + sDDEMethod
+				    + "\nsDDERule\t" + sDDERule
+				    + "\nsDDEConcatChar\t" + sDDEConcatChar
+				    + "\nsDDERepType\t" + sDDERepType);
 				cstmt.setString(1, userName); // set ua_name
 				cstmt.setString(2, sAction); // action
 				cstmt.setString(3, sP_DE_IDSEQ); // primary DE idseq
