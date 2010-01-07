a5236e245ccdc0e882eb8ffc5d17bc8e15af62b7
avdicd <avdicd@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2010-01-07 16:37:28 +0000

    SVN-Revision: 1461

--- db-sql/update_evs_url.sql
+++ db-sql/update_evs_url.sql
@@ -12,13 +12,13 @@ MERGE INTO SBREXT.TOOL_OPTIONS_VIEW_EXT S USING (
 
 --Store evs  url specific to curation tool if needed. 
 
-SELECT 'EVS' AS TOOL_NAME, 'URL' AS PROPERTY, 'http://lexevsapi.nci.nih.gov/lexevsapi42' AS VALUE, 'Store evs alternate url specific to curation tool if needed' AS DESCRIPTION FROM DUAL
+SELECT 'EVS' AS TOOL_NAME, 'URL' AS PROPERTY, 'http://lexevsapi51.nci.nih.gov/lexevsapi51' AS VALUE, 'Store evs alternate url specific to curation tool if needed' AS DESCRIPTION FROM DUAL
 UNION
-SELECT 'CURATION' AS TOOL_NAME, 'EVS.URL' AS PROPERTY, 'http://lexevsapi.nci.nih.gov/lexevsapi42' AS VALUE, 'Store evs alternate url specific to curation tool if needed' AS DESCRIPTION FROM DUAL
+SELECT 'CURATION' AS TOOL_NAME, 'EVS.URL' AS PROPERTY, 'http://lexevsapi51.nci.nih.gov/lexevsapi51' AS VALUE, 'Store evs alternate url specific to curation tool if needed' AS DESCRIPTION FROM DUAL
 )T 
 ON (S.TOOL_NAME = T.TOOL_NAME AND S.PROPERTY = T.PROPERTY) 
 WHEN MATCHED THEN 
- UPDATE SET S.DESCRIPTION = T.DESCRIPTION 
+ UPDATE SET S.DESCRIPTION = T.DESCRIPTION
 WHEN NOT MATCHED THEN INSERT (TOOL_NAME, PROPERTY, VALUE, DESCRIPTION) VALUES (T.TOOL_NAME, T.PROPERTY, T.VALUE, T.DESCRIPTION);
 
 --commit the updates 
