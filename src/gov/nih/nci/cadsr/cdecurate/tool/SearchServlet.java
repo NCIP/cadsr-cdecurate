4411b3fb14f24b93ca06df33e7c542a435bbfee5
avdicd <avdicd@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2011-05-02 01:05:46 +0000

    SVN-Revision: 1614

--- src/gov/nih/nci/cadsr/cdecurate/tool/SearchServlet.java
+++ src/gov/nih/nci/cadsr/cdecurate/tool/SearchServlet.java
@@ -2478,13 +2478,15 @@ public class SearchServlet extends CurationServlet {
     {
     	String idseq = m_classReq.getParameter("idseq");
     	String type = m_classReq.getParameter("type");
+    	
     	m_classReq.getSession().setAttribute("usedByResults", null);
     	HashMap<String,ArrayList<String[]>> crfResults = null;
     	
+    	
     	if (type != null && type.equals("PV"))
-    		crfResults = getAssociatedForms(idseq, crfResults, false);
+    		crfResults = getAssociatedForms(idseq, crfResults);
     	if (type != null && type.equals("VD"))
-    		crfResults = getAssociatedForms(idseq, crfResults, true);
+    		crfResults = getVDAssociatedForms(idseq, crfResults);
     	if (type != null && type.equals("DE"))
     		crfResults = getDEAssociatedForms(idseq, crfResults);
     	
@@ -2492,13 +2494,10 @@ public class SearchServlet extends CurationServlet {
     	ForwardJSP(m_classReq, m_classRes, "/ShowUsedBy.jsp");
     }
     
-    public HashMap<String,ArrayList<String[]>> getAssociatedForms(String vpIDseq, HashMap<String,ArrayList<String[]>> ret, boolean isVD){
+    public HashMap<String,ArrayList<String[]>> getAssociatedForms(String vpIDseq, HashMap<String,ArrayList<String[]>> ret){
 
 		String idseqString = "VP_IDSEQ";
-		if (isVD)
-			idseqString = "VD_IDSEQ";
-		//ADD DE FORMS
-		
+	
 		String sql = "SELECT DISTINCT " +
 				"crf.long_name as \"Form Name\", " +
 				"crf.public_id as \"Pub. Id\", " +
@@ -2554,6 +2553,109 @@ public class SearchServlet extends CurationServlet {
 	        		
 					while (rs.next()) {
 						String[] rowContent = new String[numColumns];
+						//TODO: Check for idseq and add the link
+						for (int i = 1; i < numColumns+1; i++)
+							rowContent[i-1] = rs.getString(i);
+						
+						usedInForms.add(rowContent);
+					}
+				
+				ret.put("Content", usedInForms);
+				}
+			}
+		} catch (Exception e) {
+			logger.error("ERROR - getAssociatedForms for other : ", e);
+		}finally{
+			rs = SQLHelper.closeResultSet(rs);
+            pstmt = SQLHelper.closePreparedStatement(pstmt);
+		}
+		
+		return ret;
+	}
+    
+    public HashMap<String,ArrayList<String[]>> getVDAssociatedForms(String vpIDseq, HashMap<String,ArrayList<String[]>> ret){
+
+		String idseqString = "VD_IDSEQ";
+		String sql = "SELECT DISTINCT " +
+				"crf.long_name as \"Form Name\", " +
+				"crf.public_id as \"Pub. Id\", " +
+				"CRF.VERSION as \"Ver.\", " +
+				"CRF.WORKFLOW as \"Workflow Status\", " +
+				"CRF.CONTEXT_NAME as \"Context\", " +
+				"UA.NAME as \"Created By\", " +
+				"CRF.CREATED_BY, " +
+				"crf.qc_idseq "
+			+" FROM"
+           +" SBREXT.FB_FORMS_VIEW crf"
+           +" ,SBREXT.QUEST_CONTENTS_VIEW_EXT qc"
+           +" ,SBR.DATA_ELEMENTS_VIEW de"
+           +" ,SBR.VALUE_DOMAINS_VIEW vd"
+           +" ,SBR.USER_ACCOUNTS_VIEW ua"
+           +" WHERE"
+           +" (qc.DE_IDSEQ = DE.DE_IDSEQ)"
+           +" AND (qc.DN_CRF_IDSEQ = crf.QC_IDSEQ)"
+           +" AND (CRF.CREATED_BY = UA.UA_NAME)"
+           +" AND (VD."+idseqString+" = ?)"
+           +" AND (VD.DE_IDSEQ = DE.DE_IDSEQ)"
+           +" order by crf.long_name desc";
+		
+		String sqlE = "SELECT DISTINCT " +
+			"crf.long_name as \"Form Name\", " +
+			"crf.public_id as \"Pub. Id\", " +
+			"CRF.VERSION as \"Ver.\", " +
+			"CRF.WORKFLOW as \"Workflow Status\", " +
+			"CRF.CONTEXT_NAME as \"Context\", " +
+			"UA.NAME as \"Created By\", " +
+			"CRF.CREATED_BY, " +
+			"crf.qc_idseq "
+			+" FROM"
+	       +" SBREXT.FB_FORMS_VIEW crf"
+	       +" ,SBREXT.QUEST_CONTENTS_VIEW_EXT qc"
+	       +" ,SBR.VD_PVS_VIEW vdpvs"
+	       +" ,SBR.USER_ACCOUNTS_VIEW ua"
+	       +" WHERE"
+	       +" (qc.VP_IDSEQ = vdpvs.VP_IDSEQ)"
+	       +" AND (qc.DN_CRF_IDSEQ = crf.QC_IDSEQ)"
+	       +" AND (CRF.CREATED_BY = UA.UA_NAME)"
+	       +" AND (VDPVS."+idseqString+" = ?)"
+	       +" order by crf.long_name desc";
+			
+		ResultSet rs = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			
+			if (vpIDseq != null && !vpIDseq.equals("")) {
+				if (this.getConn() != null) {
+					pstmt = this
+							.getConn()
+							.prepareStatement(sql);
+					// register the Out parameters
+					pstmt.setString(1, vpIDseq);
+					ArrayList<String[]> usedInForms = new ArrayList<String[]>();
+					// Now we are ready to call the function
+					rs = pstmt.executeQuery();
+					
+					ResultSetMetaData rsmd = rs.getMetaData();
+		            int numColumns = rsmd.getColumnCount();
+		            String[] headerContent = new String[numColumns];
+		    		
+		         // Get the column names; column indices start from 1
+	                for (int i=1; i<numColumns+1; i++) {
+	                    String columnName = rsmd.getColumnName(i);
+	                    headerContent[i-1] = columnName;
+	                }
+	        		//Are we adding to the existing list?	
+	        		if (ret == null) {
+	        			ret = new HashMap<String,ArrayList<String[]>>();
+	        			ArrayList<String[]> head = new ArrayList<String[]>();
+	        			head.add(headerContent);
+	        			ret.put("Head", head);
+	        		}
+	        		
+					while (rs.next()) {
+						String[] rowContent = new String[numColumns];
+						//TODO: Check for idseq and add the link
 						for (int i = 1; i < numColumns+1; i++)
 							rowContent[i-1] = rs.getString(i);
 						
