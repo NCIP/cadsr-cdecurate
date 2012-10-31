840066d941f9688daec6bf38c80815effd8f0963
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2012-10-31 14:44:17 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@1673 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

--- db-sql/update_evs_url.sql
+++ db-sql/update_evs_url.sql
@@ -1,25 +1,8 @@
-/* COPYRIGHT SCENPRO, INC, 2007
-
-   $Header: /cvsshare/content/cvsroot/cdecurate/db-sql/update_evs_url.sql,v 1.3 2009-04-20 12:49:22 hebell Exp $
-   $Name: not supported by cvs2svn $
-*/
-
---This guarantees any error will rollback the changes and exits the script so it can be trapped 
---during a build.
-whenever sqlerror exit sql.sqlcode rollback;
-
-MERGE INTO SBREXT.TOOL_OPTIONS_VIEW_EXT S USING (
-
---Store evs  url specific to curation tool if needed. 
-
-SELECT 'EVS' AS TOOL_NAME, 'URL' AS PROPERTY, 'http://lexevsapi51.nci.nih.gov/lexevsapi51' AS VALUE, 'Store evs alternate url specific to curation tool if needed' AS DESCRIPTION FROM DUAL
-UNION
-SELECT 'CURATION' AS TOOL_NAME, 'EVS.URL' AS PROPERTY, 'http://lexevsapi51.nci.nih.gov/lexevsapi51' AS VALUE, 'Store evs alternate url specific to curation tool if needed' AS DESCRIPTION FROM DUAL
-)T 
-ON (S.TOOL_NAME = T.TOOL_NAME AND S.PROPERTY = T.PROPERTY) 
-WHEN MATCHED THEN 
- UPDATE SET S.DESCRIPTION = T.DESCRIPTION
-WHEN NOT MATCHED THEN INSERT (TOOL_NAME, PROPERTY, VALUE, DESCRIPTION) VALUES (T.TOOL_NAME, T.PROPERTY, T.VALUE, T.DESCRIPTION);
-
---commit the updates 
-COMMIT;
+update sbrext.tool_options_view_ext set value = 'http://lexevsapi60.nci.nih.gov/lexevsapi60'
+where Tool_name = 'CURATION' and Property = 'EVS.URL'
+/
+update sbrext.tool_options_view_ext set value = 'http://lexevsapi60.nci.nih.gov/lexevsapi60'
+where Tool_name = 'EVSAPI' and Property = 'URL'
+/
+commit
+/
