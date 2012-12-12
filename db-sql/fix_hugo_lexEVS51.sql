07e13675b157c5ba43ec06ec5d02c2feef8dc8aa
pansu <pansu@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2012-12-12 18:07:58 +0000

    fix HUGO properties to match with LexEVS 5.1 returned values and fix display order so HGNC is before HL7
    SVN-Revision: 1739

---
+++ db-sql/fix_hugo_lexEVS51.sql
@@ -0,0 +1,45 @@
+update sbrext.tool_options_ext
+set value='HUGO Gene Nomenclature Committee'
+where property='EVS.VOCAB.10.EVSNAME';
+
+update sbrext.tool_options_ext
+set value='HGNC'
+where property='EVS.VOCAB.10.DISPLAY';
+
+update sbrext.tool_options_ext
+set property='EVS.VOCAB.09.DISPLAY'
+where value='HGNC';
+
+update sbrext.tool_options_ext
+set property='EVS.VOCAB.09.EVSNAME'
+where value='HUGO Gene Nomenclature Committee';
+
+update sbrext.tool_options_ext
+set property='EVS.VOCAB.09.VOCABCODETYPE'
+where value='HUGO_CODE';
+
+update sbrext.tool_options_ext
+set property='EVS.VOCAB.10.DISPLAY'
+where value='HL7';
+
+update sbrext.tool_options_ext
+set property='EVS.VOCAB.10.EVSNAME'
+where value='HL7 Reference Information Model';
+
+update sbrext.tool_options_ext
+set property='EVS.VOCAB.10.VOCABCODETYPE'
+where value='HL7_CODE';
+
+update sbrext.tool_options_ext
+set property='EVS.VOCAB.08.DISPLAY'
+where value='HGNC';
+
+update sbrext.tool_options_ext
+set property='EVS.VOCAB.08.EVSNAME'
+where value='HUGO Gene Nomenclature Committee';
+
+update sbrext.tool_options_ext
+set property='EVS.VOCAB.08.VOCABCODETYPE'
+where value='HUGO_CODE';
+
+commit;
