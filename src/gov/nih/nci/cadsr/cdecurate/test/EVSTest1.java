f62127695a88e938ba38dc959e17a3984e5f588e
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-01-10 20:22:08 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@1758 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

--- src/gov/nih/nci/cadsr/cdecurate/test/EVSTest1.java
+++ src/gov/nih/nci/cadsr/cdecurate/test/EVSTest1.java
@@ -45,6 +45,10 @@ import oracle.jdbc.pool.OracleDataSource;
 	//update sbrext.tool_options_view_ext set Value = 'http://lexevsapi51.nci.nih.gov/lexevsapi51' where Tool_name = 'CURATION' and Property = 'EVS.URL'
 	update sbrext.tool_options_view_ext set Value = 'http://lexevsapi60.nci.nih.gov/lexevsapi60' where Tool_name = 'EVSAPI' and Property = 'URL'
 	//update sbrext.tool_options_view_ext set Value = 'http://lexevsapi60.nci.nih.gov/lexevsapi60' where Tool_name = 'CURATION' and Property = 'EVS.URL'
+
+	select tool_name, property, VALUE from sbrext.tool_options_view_ext where Tool_name = 'CURATION' and Property = '%.INCLUDEMETA'
+	//e.g. TOOL_NAME=CURATION, PROPERTY=EVS.VOCAB.24.INCLUDEMETA, VALUE=NCI Metathesaurus
+	insert into sbrext.tool_options_view_ext (Tool_name, Property, Value) Values('CURATION', 'EVS.VOCAB.25.INCLUDEMETA', 'NCI Thesaurus')
  *
  * Setup -
  * 1. Add two arguments in Run/Debug configuration i.e.
