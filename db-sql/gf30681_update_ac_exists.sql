de6ca8e008f729b4be7afc040d2a8d098a70b39c
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-01-28 02:30:58 +0000

    GF30681 Duplicate DEC
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@1769 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

---
+++ db-sql/gf30681_update_ac_exists.sql
@@ -0,0 +1,35 @@
+/*
+ * Fix related to issue https://gforge.nci.nih.gov/tracker/index.php?func=detail&aid=30681.
+ */
+CREATE OR REPLACE FUNCTION AC_EXISTS(P_PREFERRED_NAME IN VARCHAR2
+                  ,P_CONTE_IDSEQ IN CHAR
+                  ,P_VERSION IN NUMBER
+                  ,P_ACTL_NAME IN VARCHAR2)RETURN  BOOLEAN IS
+/******************************************************************************
+   NAME:       AC_EXISTS
+   PURPOSE:    Check to see if administered component exists based on preferred name context and version
+
+   REVISIONS:
+   Ver        Date        Author           Description
+   ---------  ----------  ---------------  ------------------------------------
+   1.0        10/30/2001  Prerna Aggarwal     1. Created this function
+   1.1        1/22/2013   JT                  2. Remove check on context
+
+******************************************************************************/
+v_count  NUMBER;
+BEGIN
+SELECT COUNT(*) INTO v_count
+FROM ADMIN_COMPONENTS_VIEW
+WHERE preferred_name = P_PREFERRED_NAME
+--  AND conte_idseq= P_CONTE_IDSEQ
+AND version = P_VERSION
+AND actl_name = P_ACTL_NAME
+AND deleted_ind = 'No';
+
+IF(v_count = 1) THEN
+  RETURN  TRUE;
+ELSE
+  RETURN  FALSE;
+END IF;
+END AC_EXISTS;
+/
