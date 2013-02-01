4a928f3948a47f04ccc4b13ba51edc10f701b0a9
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-02-01 18:33:13 +0000

    SVN-Revision: 1786

--- src/gov/nih/nci/cadsr/cdecurate/tool/SearchServlet.java
+++ src/gov/nih/nci/cadsr/cdecurate/tool/SearchServlet.java
@@ -1435,7 +1435,7 @@ public class SearchServlet extends CurationServlet {
             vCompAtt.addElement("Long Name");
             vCompAtt.addElement("Public ID");
             vCompAtt.addElement("Version");
-            vCompAtt.addElement("Registration Status");//GF32398
+//            vCompAtt.addElement("Registration Status");//GF32398
             logger.info("at line 1439 of SearchServlet.java");
             vCompAtt.addElement("Workflow Status");
             vCompAtt.addElement("Context");
@@ -1460,7 +1460,7 @@ public class SearchServlet extends CurationServlet {
             vCompAtt.addElement("Long Name");
             vCompAtt.addElement("Public ID");
             vCompAtt.addElement("Version");
-            vCompAtt.addElement("Registration Status");//GF32398
+//            vCompAtt.addElement("Registration Status");//GF32398
             logger.info("at line 1464 of SearchServlet.java");
             vCompAtt.addElement("Workflow Status");
             vCompAtt.addElement("Context");
@@ -1814,7 +1814,9 @@ public class SearchServlet extends CurationServlet {
             vDefaultAttr.addElement("Long Name");
             vDefaultAttr.addElement("Public ID");
             vDefaultAttr.addElement("Version");
-            if (searchAC.equals("DataElement") || searchAC.equals("DataElementConcept") || searchAC.equals("ValueDomain"))//GF32398
+            if (searchAC.equals("DataElement") 
+//            		|| searchAC.equals("DataElementConcept") || searchAC.equals("ValueDomain")	//GF32398
+            		)
                 vDefaultAttr.addElement("Registration Status");
             vDefaultAttr.addElement("Workflow Status");
             // only if search is Data element
