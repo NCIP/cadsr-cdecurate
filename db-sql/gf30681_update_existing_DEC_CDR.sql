be0d9e884182557874ae085bf1efdcc808507883
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-02-20 19:53:55 +0000

    https://gforge.nci.nih.gov/tracker/index.php?func=detail&aid=30681 - initial script to update the existing DEC with CDR name.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@1825 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

---
+++ db-sql/gf30681_update_existing_DEC_CDR.sql
@@ -0,0 +1,56 @@
+-- run this with SBR user
+/*
+ * Notes related to issue https://gforge.nci.nih.gov/tracker/index.php?func=detail&aid=30681.
+ */
+SET SERVEROUTPUT ON;
+ 
+DECLARE
+OC_CDR_NAME VARCHAR2(100);
+PROP_CDR_NAME VARCHAR2(100);
+CDR_NAME VARCHAR2(100);
+ 
+CURSOR CDR IS
+ Select DISTINCT DEC_IDSEQ, LONG_NAME, DEC_ID  from SBR.DATA_ELEMENT_CONCEPTS 
+ where CDR_NAME is NULL 
+ and DEC_IDSEQ is not null
+and OC_IDSEQ is NOT NULL and PROP_IDSEQ is NOT NULL 
+ ;
+
+BEGIN
+ 
+For DECID in CDR LOOP
+
+begin
+Select DISTINCT name into OC_CDR_NAME from SBREXT.con_derivation_rules_ext where CONDR_IDSEQ is not null and CONDR_IDSEQ IN(Select DISTINCT CONDR_IDSEQ from 
+SBREXT.object_classes_ext where OC_IDSEQ IN(Select DISTINCT OC_IDSEQ from 
+SBR.data_element_concepts where DEC_IDSEQ =DECID.DEC_IDSEQ)
+and CONDR_IDSEQ is NOT NULL
+);
+ 
+Select DISTINCT name into PROP_CDR_NAME from SBREXT.con_derivation_rules_ext where CONDR_IDSEQ is not null and CONDR_IDSEQ IN(Select DISTINCT CONDR_IDSEQ from 
+SBREXT.properties_ext where PROP_IDSEQ IN(Select DISTINCT PROP_IDSEQ from 
+SBR.data_element_concepts where DEC_IDSEQ =DECID.DEC_IDSEQ)
+and CONDR_IDSEQ is NOT NULL
+);
+  exception
+-- handle ORA-01403: no data found here
+    when no_data_found then
+      dbms_output.put_line(CHR(10) || 'No data found for ' || DECID.LONG_NAME || ' (' || DECID.DEC_ID || ')');
+    when too_many_rows then
+      dbms_output.put_line(CHR(10) || 'Too many rows for ' || DECID.LONG_NAME || ' (' || DECID.DEC_ID || ')');
+    when others then
+      dbms_output.put_line(CHR(10) || 'Unknown error for ' || DECID.LONG_NAME || ' (' || DECID.DEC_ID || ')');
+end;
+
+-- DBMS_OUTPUT.put_line('.');
+CDR_NAME:=':' || OC_CDR_NAME || ':' || PROP_CDR_NAME;
+
+-- Update SBR.DATA_ELEMENT_CONCEPTS SET CDR_NAME=CDR_NAME where DEC_IDSEQ=DECID.DEC_IDSEQ;
+
+-- DBMS_OUTPUT.put_line('DEC_IDSEQ = ' || DECID.DEC_IDSEQ || ' LONG NAME = ' || DECID.LONG_NAME || ' CDR_NAME set to [' || CDR_NAME || ']');
+
+END LOOP;
+
+END;
+ /
+ 
