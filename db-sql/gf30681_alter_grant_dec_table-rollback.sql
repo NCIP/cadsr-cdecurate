c50fcb5ffe09091f6696b18922c8d0a24531e2fc
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-02-22 15:05:40 +0000

    SVN-Revision: 1828

---
+++ db-sql/gf30681_alter_grant_dec_table-rollback.sql
@@ -0,0 +1,10 @@
+/*
+ * Fix related to issue https://gforge.nci.nih.gov/tracker/index.php?func=detail&aid=30681.
+ */
+revoke select, insert, update on SBR.DATA_ELEMENT_CONCEPTS from cdebrowser
+/
+revoke select, insert, update on SBR.DATA_ELEMENT_CONCEPTS_VIEW from cdebrowser
+/
+ALTER TABLE SBR.DATA_ELEMENT_CONCEPTS
+  DROP COLUMN CDR_NAME;
+/
