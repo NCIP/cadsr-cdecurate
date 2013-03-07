ad53c48cf9f4051277c4d70cab53ea435ed222f3
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-03-07 21:44:05 +0000

    GF30779.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@1859 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

--- src/gov/nih/nci/cadsr/cdecurate/util/AdministeredItemUtil.java
+++ src/gov/nih/nci/cadsr/cdecurate/util/AdministeredItemUtil.java
@@ -14,4 +14,19 @@ public class AdministeredItemUtil {
 
 		return retVal;
 	}
+	
+	public static String truncateTime(String dateString) {
+		String retVal = dateString;
+		
+		//GF30779
+		if(dateString != null) {
+			int i = dateString.indexOf(" ");
+			if(i > -1) {
+				retVal = dateString.substring(0,i);
+			}
+		}
+		System.out.println("******* at line 28 of AdministeredItemUtil.java" + retVal);
+
+		return retVal;
+	}
 }
