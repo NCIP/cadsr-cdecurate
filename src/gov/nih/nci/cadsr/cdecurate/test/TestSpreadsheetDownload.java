5637227a832f674f2a2e3fe95ec440429268e84a
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-03-13 12:18:02 +0000

    SVN-Revision: 1864

--- src/gov/nih/nci/cadsr/cdecurate/test/TestSpreadsheetDownload.java
+++ src/gov/nih/nci/cadsr/cdecurate/test/TestSpreadsheetDownload.java
@@ -5,16 +5,20 @@
 
 package gov.nih.nci.cadsr.cdecurate.test;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Enumeration;
 import java.util.InvalidPropertiesFormatException;
+import java.util.Iterator;
 import java.util.Properties;
 import java.util.Vector;
 
@@ -23,6 +27,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -47,6 +59,7 @@ import gov.nih.nci.cadsr.cdecurate.tool.UtilService;
 import gov.nih.nci.cadsr.cdecurate.tool.VMAction;
 import gov.nih.nci.cadsr.cdecurate.tool.VMForm;
 import gov.nih.nci.cadsr.cdecurate.tool.VM_Bean;
+import gov.nih.nci.cadsr.cdecurate.util.AdministeredItemUtil;
 
 /**
  * @author shegde
@@ -86,6 +99,10 @@ WHERE vp.vd_idseq = vd.vd_idseq
    */
   public static void main(String[] args)
   {
+	  TestSpreadsheetDownload download=new TestSpreadsheetDownload();
+	  download.SpreadsheetDownload();
+	  System.out.println("Excel File Created");
+	  /*
 	CurationTestLogger logger1 = new CurationTestLogger(TestSpreadsheetDownload.class);
     // Initialize the Log4j environment.
     String logXML = "log4j.xml";
@@ -113,17 +130,17 @@ WHERE vp.vd_idseq = vd.vd_idseq
 		e.printStackTrace();
 	}
 
-    /*
+    
 select date_created, dec_id, long_name, cdr_name from data_element_concepts
 where dec_id = '3632820'
 order by date_created desc
-     */
+     
 //	  HttpSession session = m_classReq.getSession();
 	  InsACService ins = new InsACService(null, null, testdec.m_servlet);
 	  String testCDR = "cxxxcyyyc456";
 	  ins.checkDECUniqueCDRName(testCDR);
 //	  ins.updateDECUniqueCDRName("3632820", testCDR);
-  }
+  */}
   
   
   public void testOthers(TestSpreadsheetDownload testdec) {
@@ -1233,5 +1250,86 @@ order by date_created desc
     }
   }
 */
+
+  
+/****************************************************************************************/
+  public void SpreadsheetDownload (){
+	  
+	  ArrayList<String> allHeaders = new ArrayList<String>();
+	  FileOutputStream fileOutputStream = null;
+	  String[] FirstRow=new String[]{"data1","data2","data3","data4","data5","data6","data7","data8","data9","data10"};
+	  String[] SecondRow=new String[]{"data11","data12","data13","data14","data15","data16","data17","data18","data19","data20"};
+	  ArrayList<String[]> allRows=new ArrayList<>();
+	  allRows.add(FirstRow);
+	  allRows.add(SecondRow);
+	  String[] columns = null;
+	  
+	  for(int i=1;i<=10;i++) 
+	  allHeaders.add("Column"+i);
+	  
+	  columns = allHeaders.toArray(new String[allHeaders.size()]);
+	  int[] colIndices = new int[columns.length];
+	  for (int i=0; i < columns.length; i++) {
+			String colName = columns[i];
+				int temp = allHeaders.indexOf(colName);
+				colIndices[i]=temp;
+		}
+	  
+	  Workbook wb =  new HSSFWorkbook();
+	  Sheet sheet = wb.createSheet("Test_Sheet");
+	  Row headerRow = sheet.createRow(0);
+	  headerRow.setHeightInPoints(12.75f);
+	  for (int i = 0; i < columns.length; i++) {
+			Cell cell = headerRow.createCell(i);
+			cell.setCellValue(columns[i]);
+	  }
+	  sheet.createFreezePane(0, 1);
+	  Row row = null;
+	  Cell cell;
+	  int rownum = 1;
+	  int bump = 0;
+	  int i = 0;
+	  try {
+		  for (i = 0; i < allRows.size(); i++, rownum++) {
+				//Check if row already exists
+				int maxBump = 0;
+				if (sheet.getRow(rownum+bump) == null) {
+					row = sheet.createRow(rownum+bump);	
+				}
+				if(allRows.get(i) == null) continue;
+				for (int j = 0; j < colIndices.length; j++) {
+					cell = row.createCell(j);
+					cell.setCellValue(allRows.get(i)[colIndices[j]]);
+				}
+				bump = bump + maxBump;
+			}
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		try {
+			fileOutputStream = new FileOutputStream("C:/Test_Excel.xls");//Please specify the path
+			wb.write(fileOutputStream);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			/**
+			 * Close the fileOutputStream.
+			 */
+			try {
+				if (fileOutputStream != null) {
+					fileOutputStream.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		
+		
+		
+
+	  
+  }
+
 
 }
