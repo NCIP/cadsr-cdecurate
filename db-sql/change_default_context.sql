92c8e86e66bbfec8b81ca566dbed0464a2a17ffc
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-03-22 16:04:28 +0000

    Changed the name from NCICBIIT to just NCI as per direction of TPM.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@1901 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

---
+++ db-sql/change_default_context.sql
@@ -0,0 +1,2 @@
+update sbr.contexts_view  set name='NCI' where conte_idseq=(select tv.value from sbrext.tool_options_view_ext tv , sbr.contexts_view  cv where tv.tool_name = 'caDSR' and tv.property = 'DEFAULT_CONTEXT' and tv.value = cv.conte_idseq)
+/
