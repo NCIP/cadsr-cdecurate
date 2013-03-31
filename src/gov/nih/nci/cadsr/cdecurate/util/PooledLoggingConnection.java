b8faa3b74bc6455ae6bae68bd12b0d05c38fb5a6
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-03-31 17:36:06 +0000

    Added log4jdbc proxy feature for enhanced sql troubleshooting.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@1929 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

---
+++ src/gov/nih/nci/cadsr/cdecurate/util/PooledLoggingConnection.java
@@ -0,0 +1,53 @@
+package gov.nih.nci.cadsr.cdecurate.util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.ConnectionEventListener;
+import javax.sql.PooledConnection;
+import javax.sql.StatementEventListener;
+
+import net.sf.log4jdbc.ConnectionSpy;
+
+/**
+ * Courtesy of https://code.google.com/p/log4jdbc/wiki/DataSourceExampleForWebSphere
+ */
+public class PooledLoggingConnection implements PooledConnection {
+
+	protected PooledConnection parent;
+
+	public PooledLoggingConnection(PooledConnection pConnection) {
+		parent = pConnection;
+	}
+
+	public void close() throws SQLException {
+		parent.close();
+	}
+
+	public Connection getConnection() throws SQLException {
+		return new ConnectionSpy(parent.getConnection()); // -- log4jdbc entry
+															// point!!!
+	}
+
+	@Override
+	public void addConnectionEventListener(ConnectionEventListener pListener) {
+		parent.addConnectionEventListener(pListener);
+	}
+
+	@Override
+	public void addStatementEventListener(StatementEventListener arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeConnectionEventListener(ConnectionEventListener pListener) {
+		parent.removeConnectionEventListener(pListener);
+	}
+
+	@Override
+	public void removeStatementEventListener(StatementEventListener arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
--- testlib/readme.txt
+++ testlib/readme.txt
@@ -3,3 +3,7 @@ Notes:
 junit/*.jar is needed by CDECurateWebTest.java.
 
 LexEVS_60_client_lib/*.jar is required by LexEVS client test program, SearchByName.java and SearchPropertiesByConceptCode.java.
+
+*** JDBC proxy driver for logging SQL and other interesting information ***
+
+The proxy is included but currently not activated. For more information, please check https://code.google.com/p/log4jdbc/.
