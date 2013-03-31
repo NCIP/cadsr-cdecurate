f9d1e9ddb8093954ba1efb6d2cfdb3c8642b2d25
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-03-31 17:36:06 +0000

    Added log4jdbc proxy feature for enhanced sql troubleshooting.
    SVN-Revision: 1929

---
+++ src/gov/nih/nci/cadsr/cdecurate/util/OracleLoggingConnectionPooledDataSource.java
@@ -0,0 +1,41 @@
+package gov.nih.nci.cadsr.cdecurate.util;
+
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.sql.PooledConnection;
+
+import org.apache.log4j.PropertyConfigurator;
+
+import oracle.jdbc.pool.OracleConnectionPoolDataSource;
+
+public class OracleLoggingConnectionPooledDataSource extends
+		OracleConnectionPoolDataSource {
+
+	public OracleLoggingConnectionPooledDataSource()
+			throws java.sql.SQLException {
+		super();
+		initLogging();
+	}
+
+	protected void initLogging() {
+		try {
+			PropertyConfigurator.configure(getClass().getClassLoader()
+					.getResource("log4jdbc_log4j.properties"));
+		} catch (Exception pEx) {
+			System.err.println("Error configuring log4jdbc logging: "
+					+ pEx.toString());
+		}
+	}
+
+	public PooledConnection getPooledConnection(Properties arg0)
+			throws SQLException {
+		return new PooledLoggingConnection(getPooledConnection(arg0));
+	}
+
+	public PooledConnection getPooledConnection(String arg0, String arg1)
+			throws SQLException {
+		return new PooledLoggingConnection(
+				super.getPooledConnection(arg0, arg1));
+	}
+}
