d454e8074325a286c4e17915c254b0094eb41738
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-04-02 21:14:37 +0000

    From: Warzel, Denise (NIH/NCI) [E] Sent: Thursday, March 28, 2013 12:58 PM To: Gaheen, Sharon M. Subject: Context Support Issue
    SVN-Revision: 1941

--- db-sql/delete_context_HITSP.sql
+++ db-sql/delete_context_HITSP.sql
@@ -1,19 +1,21 @@
 --Run with SBREXT user
-select * from SBR.PROGRAM_AREAS_LOV
-/
-select * from SBR.LIFECYCLES_LOV
-/
-delete from SBR.PROGRAM_AREAS_LOV where PAL_NAME = 'UNASSIGNED'
-/
-delete from SBR.LIFECYCLES_LOV where LL_NAME = 'UNASSIGNED'
-/
-select * from SBR.PROGRAM_AREAS_LOV
-/
-select * from SBR.LIFECYCLES_LOV
-/
-select name, PAL_NAME, LL_NAME from SBR.CONTEXTS where NAME = 'HITSP'
-/
-delete from SBR.CONTEXTS where NAME = 'HITSP'
-/
-select name, PAL_NAME, LL_NAME from SBR.CONTEXTS where NAME = 'HITSP'
-/
+
+--Prove that it exist first :<
+select CONTE_IDSEQ, DESCRIPTION, pal_name, ll_name from SBR.CONTEXTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
+
+--DELETE AC's child first
+delete from SBR.AC_CSI where AC_IDSEQ in (select AC_IDSEQ from ADMINISTERED_COMPONENTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP'));
+
+--DELETE AC
+delete from SBR.ADMINISTERED_COMPONENTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
+
+delete from SBR.CS_CSI where CS_IDSEQ = (select CS_IDSEQ from SBR.CLASSIFICATION_SCHEMES where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP'));
+
+delete from SBR.CLASSIFICATION_SCHEMES where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
+
+delete from CONTEXTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
+
+--Prove that it is gone :)
+select CONTE_IDSEQ, DESCRIPTION, pal_name, ll_name from SBR.CONTEXTS where NAME = 'HITSP';
+
+commit;
