99e057214be8b197af54d56576a74f8877db9c44
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-04-03 21:31:57 +0000

    SVN-Revision: 1948

--- db-sql/delete_context_HITSP.sql
+++ db-sql/delete_context_HITSP.sql
@@ -1,10 +1,18 @@
 --Run with SBREXT user
 
+select * from SBR.ADMINISTERED_COMPONENTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP')
+
+select * from SBR.VALUE_DOMAINS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP')
+
+
 --Prove that it exist first :<
 select CONTE_IDSEQ, DESCRIPTION, pal_name, ll_name from SBR.CONTEXTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
 
 --DELETE AC's child first
 delete from SBR.AC_CSI where AC_IDSEQ in (select AC_IDSEQ from ADMINISTERED_COMPONENTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP'));
+
+--additional STAGE tier specific delete (4/3/2013)
+delete from SBR.DESIGNATIONS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
 
 --DELETE AC
 delete from SBR.ADMINISTERED_COMPONENTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
@@ -12,12 +20,15 @@ delete from SBR.ADMINISTERED_COMPONENTS where CONTE_IDSEQ = (select CONTE_IDSEQ 
 delete from SBR.CS_CSI where CS_IDSEQ = (select CS_IDSEQ from SBR.CLASSIFICATION_SCHEMES where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP'));
 
 --additional STAGE tier specific delete (4/3/2013)
-delete from SBR.DEFINITIONS WHERE CONTE_IDSEQ = (SELECT CONTE_IDSEQ FROM SBR.CONTEXTS WHERE name = 'HITSP')
+delete from SBR.DEFINITIONS WHERE CONTE_IDSEQ = (SELECT CONTE_IDSEQ FROM SBR.CONTEXTS WHERE name = 'HITSP');
 
 delete from SBR.CLASSIFICATION_SCHEMES where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
 
 --additional QA tier specific delete (4/3/2013)
-delete SBR.VD_PVS WHERE CONTE_IDSEQ = (SELECT CONTE_IDSEQ FROM SBR.CONTEXTS WHERE name = 'HITSP')
+delete SBR.VD_PVS WHERE CONTE_IDSEQ = (SELECT CONTE_IDSEQ FROM SBR.CONTEXTS WHERE name = 'HITSP');
+
+--additional STAGE tier specific delete (4/3/2013)
+delete from SBR.VALUE_DOMAINS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
 
 delete from SBR.CONTEXTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
 
