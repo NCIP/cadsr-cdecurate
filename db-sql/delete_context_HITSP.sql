35f9e36c78a3ad9caf09f1b9c5630ab9ffd45df9
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-04-04 15:41:46 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@1955 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

--- db-sql/delete_context_HITSP.sql
+++ db-sql/delete_context_HITSP.sql
@@ -1,16 +1,25 @@
 --Run with SBREXT user
 
+--Confirming what we have first
+select 1 from dual;
 select * from SBR.ADMINISTERED_COMPONENTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
 
+select 2 from dual;
 select * from SBR.VALUE_DOMAINS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
 
+select 3 from dual;
 select * from SBR.REFERENCE_DOCUMENTS where AC_IDSEQ in (select AC_IDSEQ from ADMINISTERED_COMPONENTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP'));
 
+select 4 from dual;
 select * from SBR.DATA_ELEMENTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
 
+select 5 from dual;
+select * from SBR.DATA_ELEMENT_CONCEPTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
+
+select 6 from dual;
 select * from SBR.SC_CONTEXTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
 
---Prove that it exist first :<
+select 7 from dual;
 select CONTE_IDSEQ, DESCRIPTION, pal_name, ll_name from SBR.CONTEXTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
 
 --DELETE AC's child first
@@ -18,6 +27,9 @@ delete from SBR.AC_CSI where AC_IDSEQ in (select AC_IDSEQ from ADMINISTERED_COMP
 
 --additional STAGE tier specific delete (4/3/2013)
 delete from SBR.DESIGNATIONS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
+
+--DELETE SC token
+delete from SBREXT.GS_TOKENS where AC_IDSEQ in (select AC_IDSEQ from ADMINISTERED_COMPONENTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP'));
 
 --DELETE AC
 delete from SBR.ADMINISTERED_COMPONENTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
@@ -40,6 +52,9 @@ delete from SBR.DATA_ELEMENTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.C
 
 --additional STAGE tier specific delete (4/3/2013)
 delete from SBR.VALUE_DOMAINS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
+
+--additional STAGE tier specific delete (4/4/2013)
+delete from SBR.DATA_ELEMENT_CONCEPTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
 
 --additional STAGE tier specific delete (4/4/2013)
 delete from SBR.SC_CONTEXTS where CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'HITSP');
