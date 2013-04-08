dd243ff8700b554e17ccb3678acb3b19b03edd54
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-04-08 23:29:28 +0000

    SVN-Revision: 1967

--- WebRoot/jsp/crud.js
+++ WebRoot/jsp/crud.js
@@ -1,15 +1,11 @@
-// AngularJS Stuff
-var myApp = angular.module('app', [], ctrlRead);
-
 function ctrlRead($scope, $filter) {
     // app specific init
-    //$scope.title = "Custom Profile";
+    $scope.title = "Custom Download Profile";
     $scope.createLinkTitle = "New Profile";
     $scope.header1 = "Action";
     $scope.header2 = "ID";
     $scope.header3 = "Name";
     $scope.header4 = "Description";
-    $scope.enterNew = true;
     // common init
     $scope.sortingOrder = sortingOrder;
     $scope.reverse = false;
@@ -19,26 +15,26 @@ function ctrlRead($scope, $filter) {
     $scope.pagedItems = [];
     $scope.currentPage = 0;
     $scope.items = [
-        {"id":"1","name":"name 1","description":"description 1","createDate":"","field4":"field4 1","field5 ":"field5 1"},
-        {"id":"2","name":"name 2","description":"description 2","createDate":"field3 2","field4":"field4 2","field5 ":"field5 2"},
-        {"id":"3","name":"name 3","description":"description 3","createDate":"field3 3","field4":"field4 3","field5 ":"field5 3"},
-        {"id":"4","name":"name 4","description":"description 4","createDate":"field3 4","field4":"field4 4","field5 ":"field5 4"},
-        {"id":"5","name":"name 5","description":"description 5","createDate":"field3 5","field4":"field4 5","field5 ":"field5 5"},
-        {"id":"6","name":"name 6","description":"description 6","createDate":"field3 6","field4":"field4 6","field5 ":"field5 6"},
-        {"id":"7","name":"name 7","description":"description 7","createDate":"field3 7","field4":"field4 7","field5 ":"field5 7"},
-        {"id":"8","name":"name 8","description":"description 8","createDate":"field3 8","field4":"field4 8","field5 ":"field5 8"},
-        {"id":"9","name":"name 9","description":"description 9","createDate":"field3 9","field4":"field4 9","field5 ":"field5 9"},
-        {"id":"10","name":"name 10","description":"description 10","createDate":"field3 10","field4":"field4 10","field5 ":"field5 10"},
-        {"id":"11","name":"name 11","description":"description 11","createDate":"field3 11","field4":"field4 11","field5 ":"field5 11"},
-        {"id":"12","name":"name 12","description":"description 12","createDate":"field3 12","field4":"field4 12","field5 ":"field5 12"},
-        {"id":"13","name":"name 13","description":"description 13","createDate":"field3 13","field4":"field4 13","field5 ":"field5 13"},
-        {"id":"14","name":"name 14","description":"description 14","createDate":"field3 14","field4":"field4 14","field5 ":"field5 14"},
-        {"id":"15","name":"name 15","description":"description 15","createDate":"field3 15","field4":"field4 15","field5 ":"field5 15"},
-        {"id":"16","name":"name 16","description":"description 16","createDate":"field3 16","field4":"field4 16","field5 ":"field5 16"},
-        {"id":"17","name":"name 17","description":"description 17","createDate":"field3 17","field4":"field4 17","field5 ":"field5 17"},
-        {"id":"18","name":"name 18","description":"description 18","createDate":"field3 18","field4":"field4 18","field5 ":"field5 18"},
-        {"id":"19","name":"name 19","description":"description 19","createDate":"field3 19","field4":"field4 19","field5 ":"field5 19"},
-        {"id":"20","name":"name 20","description":"description 20","createDate":"field3 20","field4":"field4 20","field5 ":"field5 20"}
+        {"id":"1","name":"name 1","description":"description 1","field3":"field3 1","field4":"field4 1","field5 ":"field5 1"},
+        {"id":"2","name":"name 2","description":"description 1","field3":"field3 2","field4":"field4 2","field5 ":"field5 2"},
+        {"id":"3","name":"name 3","description":"description 1","field3":"field3 3","field4":"field4 3","field5 ":"field5 3"},
+        {"id":"4","name":"name 4","description":"description 1","field3":"field3 4","field4":"field4 4","field5 ":"field5 4"},
+        {"id":"5","name":"name 5","description":"description 1","field3":"field3 5","field4":"field4 5","field5 ":"field5 5"},
+        {"id":"6","name":"name 6","description":"description 1","field3":"field3 6","field4":"field4 6","field5 ":"field5 6"},
+        {"id":"7","name":"name 7","description":"description 1","field3":"field3 7","field4":"field4 7","field5 ":"field5 7"},
+        {"id":"8","name":"name 8","description":"description 1","field3":"field3 8","field4":"field4 8","field5 ":"field5 8"},
+        {"id":"9","name":"name 9","description":"description 1","field3":"field3 9","field4":"field4 9","field5 ":"field5 9"},
+        {"id":"10","name":"name 10","description":"description 1","field3":"field3 10","field4":"field4 10","field5 ":"field5 10"},
+        {"id":"11","name":"name 11","description":"description 1","field3":"field3 11","field4":"field4 11","field5 ":"field5 11"},
+        {"id":"12","name":"name 12","description":"description 1","field3":"field3 12","field4":"field4 12","field5 ":"field5 12"},
+        {"id":"13","name":"name 13","description":"description 1","field3":"field3 13","field4":"field4 13","field5 ":"field5 13"},
+        {"id":"14","name":"name 14","description":"description 1","field3":"field3 14","field4":"field4 14","field5 ":"field5 14"},
+        {"id":"15","name":"name 15","description":"description 1","field3":"field3 15","field4":"field4 15","field5 ":"field5 15"},
+        {"id":"16","name":"name 16","description":"description 1","field3":"field3 16","field4":"field4 16","field5 ":"field5 16"},
+        {"id":"17","name":"name 17","description":"description 1","field3":"field3 17","field4":"field4 17","field5 ":"field5 17"},
+        {"id":"18","name":"name 18","description":"description 1","field3":"field3 18","field4":"field4 18","field5 ":"field5 18"},
+        {"id":"19","name":"name 19","description":"description 1","field3":"field3 19","field4":"field4 19","field5 ":"field5 19"},
+        {"id":"20","name":"name 20","description":"description 1","field3":"field3 20","field4":"field4 20","field5 ":"field5 20"}
     ];
 
     var searchMatch = function (haystack, needle) {
@@ -78,6 +74,7 @@ function ctrlRead($scope, $filter) {
             }
         }
     };
+    var loadItems = $scope.groupToPages;
 
     $scope.range = function (start, end) {
         var ret = [];
@@ -131,14 +128,12 @@ function ctrlRead($scope, $filter) {
     $scope.newItem = function() {
         $scope.enterNew = true;
         $scope.editing = false;
-        $scope.copying = false;
-        $scope.item = {};
+        $scope.student = {};
     };
 
     $scope.createItem = function() {
-        $scope.item.createdDate = new Date();
         $scope.pagedItems[$scope.currentPage].push($scope.item);
-        //$scope.items.push($scope.item);
+        $scope.items.push($scope.item);
 
         /*
          $http.post('/students.json', {"student": $scope.student})
@@ -151,55 +146,21 @@ function ctrlRead($scope, $filter) {
          $scope.error_message = response.error_message;
          });
          */
-
-        $scope.enterNew = true;
-        $scope.editing = false;
-        $scope.copying = false;
-        $scope.item = "";
-        //$scope.groupToPages();
     }
 
     $scope.cancelSave = function() {
-        $scope.enterNew = true;
+        $scope.enterNew = false;
         $scope.editing = false;
-        $scope.copying = false;
         $scope.item = {};
     };
 
     $scope.editItem = function(item) {
         $scope.enterNew = false;
         $scope.editing = true;
-        $scope.copying = false;
         $scope.item = item;
     };
 
-    $scope.copyItem = function(item) {
-        $scope.enterNew = false;
-        $scope.editing = false;
-        $scope.copying = true;
-        
-        newItem = {};
-        newItem.name = item.name;
-        newItem.description = item.description;
-        newItem.createDate = new Date();
-        
-        $scope.newItem = newItem;
-        $scope.item = newItem;
-    };
-
-    $scope.cloneItem = function(item) {
-        $scope.pagedItems[$scope.currentPage].push($scope.newItem);
-        
-        $scope.enterNew = true;
-        $scope.editing = false;
-        $scope.copying = false;
-        
-        $scope.item = "";
-    };
-
     $scope.updateItem = function() {
-        $scope.item.createdDate = new Date();
-
 /*
         $http.put('/students/' + $scope.student.id + '.json', {"student": $scope.student})
             .success(function(response, status, headers, config){
@@ -211,10 +172,6 @@ function ctrlRead($scope, $filter) {
                 $scope.error_message = response.error_message;
             });
 */
-        $scope.enterNew = true;
-        $scope.editing = false;
-        $scope.copying = false;
-        $scope.item = "";
     };
 
     $scope.deleteItem = function(item) {
@@ -236,6 +193,5 @@ function ctrlRead($scope, $filter) {
     }
 
 };
-
 ctrlRead.$inject = ['$scope', '$filter'];
 
