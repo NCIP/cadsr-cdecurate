f8766521b49a49b18e18bd4fa3e01121840f995f
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-05-03 16:45:13 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@2061 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

--- db-sql/change_user_groups_sc_groups_to_NCIP_cleanup.sql
+++ db-sql/change_user_groups_sc_groups_to_NCIP_cleanup.sql
@@ -1,7 +1,13 @@
 --Run with SBR user
+DROP TABLE SBR.SECURITY_CONTEXTS_LOV_BACKUP;
+
+DROP TABLE sbr.groups_backup;
+
 DROP TABLE sbr.user_groups_backup;
 
 DROP TABLE sbr.sc_groups_backup;
+
+desc sbr.groups_backup;
 
 desc sbr.user_groups_backup;
 
