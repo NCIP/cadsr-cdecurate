5ed80ce24c272854e1ea59ac1e2fe352ea8d0fc6
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-05-07 19:21:03 +0000

    Defined NCIP as a constant.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@2070 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

--- src/gov/nih/nci/cadsr/common/Constants.java
+++ src/gov/nih/nci/cadsr/common/Constants.java
@@ -1,6 +1,7 @@
 package gov.nih.nci.cadsr.common;
 
 public class Constants {
+	public static final String DEFAULT_CONTEXT = "NCIP";
 	public static final String DEFAULT_MANDATORY_ATTRIBUTE_TEXT = "This field is Mandatory. \n";
 	public static final String DEC_CDR_NAME = "dec_cdr_name";
 }
