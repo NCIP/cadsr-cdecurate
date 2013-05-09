cafb203bac45509aa67c37c81e4f2324a3a35f05
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-05-09 18:22:40 +0000

    More sanity checks after the change.
    SVN-Revision: 2072

--- db-sql/change_default_context_to_NCIP.sql
+++ db-sql/change_default_context_to_NCIP.sql
@@ -3,5 +3,16 @@ update sbr.contexts_view  set name='NCIP' where conte_idseq=(select tv.value fro
 /
 commit
 /
-select name from sbr.contexts_view where conte_idseq=(select tv.value from sbrext.tool_options_view_ext tv , sbr.contexts_view  cv where tv.tool_name = 'caDSR' and tv.property = 'DEFAULT_CONTEXT' and tv.value = cv.conte_idseq);
+-- To Check whether default context is changed to "NCIP" in Database
+SELECT name from sbr.contexts_view where conte_idseq=(select tv.value from sbrext.tool_options_view_ext tv , sbr.contexts_view  cv where 
+tv.tool_name = 'caDSR' and tv.property = 'DEFAULT_CONTEXT' and tv.value = cv.conte_idseq)
+/
+-- To Check whether DE's exists with "NCIP" context in Database
+SELECT count(*) FROM sbr.data_elements where conte_idseq = (select CONTE_IDSEQ from SBR.contexts where name='NCIP')
+/
+-- To Check whether DEC's exists with "NCIP" context in Database
+SELECT count(*) FROM sbr.data_element_concepts where conte_idseq = (select CONTE_IDSEQ from SBR.contexts where name='NCIP')
+/
+-- To Check whether VD's exists with "NCIP" context in Database
+SELECT count(*) FROM sbr.value_domains where conte_idseq = (select CONTE_IDSEQ from SBR.contexts where name='NCIP')
 /
