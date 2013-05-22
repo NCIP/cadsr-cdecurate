16dfe55afe0a6fd2322a83fc181f0fc693e1b5e1
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-05-22 16:07:59 +0000

    SVN-Revision: 2083

--- db-sql/create_new_contexts/create_context.sh
+++ db-sql/create_new_contexts/create_context.sh
@@ -1,11 +1,11 @@
-#Please run with user SBREXT
+#Please run with user SBR
 export user=SBR
 export pwd=jjsbr
 export SQLPLUS_PATH=~/Dropbox/nih/instantclient_10_2/
 #export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=137.187.181.4)(port=1551)))(connect_data=(SID=DSRDEV)))"
 
-$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @run_create_context.sql 'NHC-NCI' '(Norton Cancer Institute)'
-$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @run_create_context.sql 'PBTC' '(Pediatric Brain Tumor Consortium)'
-$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @run_create_context.sql 'CITN' '(Cancer Immunotherapy Trials Network)'
-$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @run_create_context.sql 'OHSU Knight' '(Oregon Health & Science University Knight Cancer Institute)'
+$SQLPLUS_PATHsqlplus $user/$pwd$HOST @run_create_context.sql 'NHC-NCI' '(Norton Cancer Institute)'
+$SQLPLUS_PATHsqlplus $user/$pwd$HOST @run_create_context.sql 'PBTC' '(Pediatric Brain Tumor Consortium)'
+$SQLPLUS_PATHsqlplus $user/$pwd$HOST @run_create_context.sql 'CITN' '(Cancer Immunotherapy Trials Network)'
+$SQLPLUS_PATHsqlplus $user/$pwd$HOST @run_create_context.sql 'OHSU Knight' '(Oregon Health & Science University Knight Cancer Institute)'
 
