5a2aaf8ea76bd31e7397ce09aab6513580e3ddb5
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-05-24 20:02:51 +0000

    Initial completed codes.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@2089 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

--- db-sql/change_user_context/change_context.sh
+++ db-sql/change_user_context/change_context.sh
@@ -1,11 +1,15 @@
+#!/bin/sh
 #Please run with user SBR
 echo input parameters in the order specified:
-echo old_context to new_context from_date(format YYYY-MM-DD) end_date (format YYYY-MM-DD) curator_id
+echo "old_context to new_context from_date(format YYYY-MM-DD) end_date (format YYYY-MM-DD) curator_id"
 export user=SBR
 export pwd=jjsbr
-#export SQLPLUS_PATH=~/Dropbox/nih/instantclient_10_2/
-export SQLPLUS_PATH=
-#export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=137.187.181.4)(port=1551)))(connect_data=(SID=DSRDEV)))"
+export SQLPLUS_PATH=~/Dropbox/nih/instantclient_10_2
+#export SQLPLUS_PATH=
+export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=137.187.181.4)(port=1551)))(connect_data=(SID=DSRDEV)))"
 
-$SQLPLUS_PATHsqlplus $user/$pwd$HOST @change_user_ac_context.sql 'NIDA' 'NHLBI' '2013-03-01' '2013-03-22'
-$SQLPLUS_PATHsqlplus $user/$pwd$HOST @change_user_ac_context.sql 'NIDA' 'NHLBI' '2013-05-22' '2013-05-22'
+#$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @change_user_ac_context.sql 'NCIP' 'NHLBI' '2013-04-01' '2013-04-30' xxx
+#$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @check_user_ac_context.sql 'NCIP' 'NHLBI' '2013-04-01' '2013-04-30' xxx
+
+#$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @change_user_ac_context.sql 'NIDA' 'NHLBI' '2013-05-01' '2013-05-22' xxx
+#$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @check_user_ac_context.sql 'NIDA' 'NHLBI' '2013-05-01' '2013-05-22' xxx
--- db-sql/change_user_context/change_user_ac_context.sql
+++ db-sql/change_user_context/change_user_ac_context.sql
@@ -9,9 +9,32 @@ AS (SELECT *
 select count(*) from SBR.AC_BACKUP1;
 */
 
-update SBR.ADMINISTERED_COMPONENTS ac 
-set ac.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = '&2'),
+update sbr.data_element_concepts dec
+set dec.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = '&2')
+where 
+dec.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = '&1')
+and (upper(dec.created_by) = '&5' or lower(dec.created_by) = '&5')
+and trunc(dec.date_created) >= to_date('&3', 'YYYY-MM-DD')
+and trunc(dec.date_created) <= to_date('&4', 'YYYY-MM-DD');
 
+update sbr.data_elements de
+set de.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = '&2')
+where 
+de.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = '&1')
+and (upper(de.created_by) = '&5' or lower(de.created_by) = '&5')
+and trunc(de.date_created) >= to_date('&3', 'YYYY-MM-DD')
+and trunc(de.date_created) <= to_date('&4', 'YYYY-MM-DD');
+
+update sbr.value_domains vd
+set vd.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = '&2')
+where 
+vd.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = '&1')
+and (upper(vd.created_by) = '&5' or lower(vd.created_by) = '&5')
+and trunc(vd.date_created) >= to_date('&3', 'YYYY-MM-DD')
+and trunc(vd.date_created) <= to_date('&4', 'YYYY-MM-DD');
+
+update SBR.ADMINISTERED_COMPONENTS ac 
+set ac.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = '&2')
 where ac.public_id in (
 select 
 --ac.date_created, ac.actl_name, 
