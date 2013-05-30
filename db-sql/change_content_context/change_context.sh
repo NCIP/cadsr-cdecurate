9b5916be02050a07cf899d3ecdacc8fe0e7fc05b
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-05-30 15:31:46 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@2091 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

--- db-sql/change_content_context/change_context.sh
+++ db-sql/change_content_context/change_context.sh
@@ -3,7 +3,7 @@
 echo input parameters in the order specified:
 echo "old_context to new_context from_date(format YYYY-MM-DD) end_date (format YYYY-MM-DD) curator_id"
 export user=SBR
-export pwd=jjsbr
+export pwd=
 export SQLPLUS_PATH=~/Dropbox/nih/instantclient_10_2
 #export SQLPLUS_PATH=
 export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=137.187.181.4)(port=1551)))(connect_data=(SID=DSRDEV)))"
@@ -11,5 +11,5 @@ export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=137.187.18
 #$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @change_user_ac_context.sql 'NCIP' 'NHLBI' '2013-04-01' '2013-04-30' xxx
 #$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @check_user_ac_context.sql 'NCIP' 'NHLBI' '2013-04-01' '2013-04-30' xxx
 
-$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @change_user_ac_context.sql 'NIDA' 'NHLBI' '2013-05-22' '2013-05-23' DWARZEL
-$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @check_user_ac_context.sql 'NIDA' 'NHLBI' '2013-05-22' '2013-05-23' DWARZEL
+$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @change_user_ac_context.sql 'NIDA' 'NHLBI' '2013-05-22' '2013-05-23' xxx
+$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @check_user_ac_context.sql 'NIDA' 'NHLBI' '2013-05-22' '2013-05-23' xxx
