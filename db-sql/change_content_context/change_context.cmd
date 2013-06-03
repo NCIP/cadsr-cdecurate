979322cc86ba2e73aa075b786ff53aa657043ea3
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-06-03 15:23:47 +0000

    Added timestamps, creator's etc.
    SVN-Revision: 2093

--- db-sql/change_content_context/change_context.cmd
+++ db-sql/change_content_context/change_context.cmd
@@ -7,5 +7,11 @@ set SQLPLUS_PATH=C:\instantclient_11_2\
 :set SQLPLUS_PATH=
 set HOST=@"(description=(address_list=(address=(protocol=TCP)(host=137.187.181.4)(port=1551)))(connect_data=(SID=DSRDEV)))"
 
-%SQLPLUS_PATH%sqlplus %user%/%pwd%%HOST% @change_user_ac_context.sql 'NIDA' 'NHLBI' '2013-03-01' '2013-03-22' 'dwarzel'
-:%SQLPLUS_PATH%sqlplus %user%/%pwd%%HOST% @change_user_ac_context.sql 'NIDA' 'NHLBI' '2013-05-22' '2013-05-22' 'dwarzel'
+set FROM_CONTEXT=NHLBI
+set TO_CONTEXT=NHLBI
+set FROM_DATE=2013-05-22
+set TO_DATE=2013-05-23
+set CREATOR=DWARZEL
+
+%SQLPLUS_PATH%sqlplus %user%/%pwd%%HOST% @change_user_ac_context.sql '%FROM_CONTEXT%' '%TO_CONTEXT%' '%FROM_DATE%' '%TO_DATE%' '%CREATOR%' 'Changed from [%FROM_CONTEXT%] to [%TO_CONTEXT%] for [%CREATOR%] by script [%DATE% %TIME%] '
+%SQLPLUS_PATH%sqlplus %user%/%pwd%%HOST% @check_user_ac_context.sql '%FROM_CONTEXT%' '%TO_CONTEXT%' '%FROM_DATE%' '%TO_DATE%' '%CREATOR%'
