110952897d3126adda0402655710ebb4329e919a
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-06-11 12:36:28 +0000

    Remove () from descriptions based on sponsor's feedback.
    SVN-Revision: 2101

--- db-sql/create_new_contexts/create_context.sh
+++ db-sql/create_new_contexts/create_context.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 #Please run with user SBR
+#----------------------------------------------------------------------------------------------------------------------------------
+#	DATABASE SETTINGS
+
 export user=SBR
 export pwd=jjsbr
 export SQLPLUS_PATH=~/Dropbox/nih/instantclient_10_2/
 #export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=137.187.181.4)(port=1551)))(connect_data=(SID=DSRDEV)))"
 
-$SQLPLUS_PATHsqlplus $user/$pwd$HOST @run_create_context.sql 'NHC-NCI' '(Norton Cancer Institute)'
-$SQLPLUS_PATHsqlplus $user/$pwd$HOST @run_create_context.sql 'PBTC' '(Pediatric Brain Tumor Consortium)'
-$SQLPLUS_PATHsqlplus $user/$pwd$HOST @run_create_context.sql 'CITN' '(Cancer Immunotherapy Trials Network)'
-$SQLPLUS_PATHsqlplus $user/$pwd$HOST @run_create_context.sql 'OHSU Knight' '(Oregon Health & Science University Knight Cancer Institute)'
+
+$SQLPLUS_PATHsqlplus $user/$pwd$HOST @run_create_context.sql 'NHC-NCI' 'Norton Cancer Institute'
+$SQLPLUS_PATHsqlplus $user/$pwd$HOST @run_create_context.sql 'PBTC' 'Pediatric Brain Tumor Consortium'
+$SQLPLUS_PATHsqlplus $user/$pwd$HOST @run_create_context.sql 'CITN' 'Cancer Immunotherapy Trials Network'
+$SQLPLUS_PATHsqlplus $user/$pwd$HOST @run_create_context.sql 'OHSU Knight' 'Oregon Health & Science University Knight Cancer Institute'
 
