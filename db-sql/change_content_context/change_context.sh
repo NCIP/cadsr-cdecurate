d33708dd5970317c691044f8ea85d8b1b940cdb0
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-06-13 13:05:10 +0000

    Changed based on input from DBA.
    SVN-Revision: 2104

--- db-sql/change_content_context/change_context.sh
+++ db-sql/change_content_context/change_context.sh
@@ -6,8 +6,8 @@ echo input parameters in the order specified:
 echo "old_context to new_context from_date(format YYYY-MM-DD) end_date (format YYYY-MM-DD) curator_id"
 export user=SBR
 export pwd=xxx
-export SQLPLUS_PATH=~/Dropbox/nih/instantclient_10_2
-#export SQLPLUS_PATH=
+#export SQLPLUS_PATH=~/Dropbox/nih/instantclient_10_2/
+export SQLPLUS_PATH=
 export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=137.187.181.4)(port=1551)))(connect_data=(SID=DSRDEV)))"
 
 #************* APPLICATION SETTINS *************
@@ -23,9 +23,4 @@ export DATE_TIME=$(date)
 export CHANGE_NOTE="'Changed from $FROM_CONTEXT to $TO_CONTEXT for $CREATOR by script $DATE_TIME'"
 #echo $CHANGE_NOTE
 #exit
-#$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @change_user_ac_context.sql 'NHLBI' 'NHLBI' '2013-05-22' '2013-05-23' xxx
-#$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @check_user_ac_context.sql 'NHLBI' 'NHLBI' '2013-05-22' '2013-05-23' xxx
-
-$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR $CHANGE_NOTE
-#$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR 'Test'
-$SQLPLUS_PATH/sqlplus $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
+$SQLPLUS_PATHsqlplus $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
