13225b0a75e2c3af2a3f7bad34b04cb301ed85d5
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-06-20 16:08:58 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@2108 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

---
+++ db-sql/change_content_context/sample_check_affected_content.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#************* DATABASE  SETTINS *************
+export user=SBR
+export pwd=jjuser
+export SQLPLUS_PATH=~/Dropbox/nih/instantclient_10_2/
+#export SQLPLUS_PATH=
+export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=137.187.181.4)(port=1551)))(connect_data=(SID=DSRDEV)))"
+#export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=ncidb-dsr-p.nci.nih.gov)(port=1551)))(connect_data=(SID=DSRPROD)))"
+ 
+#************* APPLICATION SETTINS *************
+#NONE
+ 
+$SQLPLUS_PATHsqlplus $user/$pwd$HOST @check_user_ac_context.sql 'CTEP' 'SWOG' '2001-12-01' '2013-06-19' 'KEARNSD' > /tmp/check_ac_counts.txt 2>&1
+ 
+$SQLPLUS_PATHsqlplus $user/$pwd$HOST @check_user_ac_context.sql 'CTEP' 'SWOG' '2001-12-01' '2013-06-19' 'SMITHA' >> /tmp/check_ac_counts.txt 2>&1
+ 
+$SQLPLUS_PATHsqlplus $user/$pwd$HOST @check_user_ac_context.sql 'CTEP' 'SWOG' '2001-12-01' '2013-06-19' 'SCOTTM' >> /tmp/check_ac_counts.txt 2>&1
