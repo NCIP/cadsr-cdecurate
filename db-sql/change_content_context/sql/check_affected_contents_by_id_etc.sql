f435dc6c4711f157a333ad7389c895a33d5d7be1
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-06-25 22:39:43 +0000

    SVN-Revision: 2112

--- db-sql/change_content_context/sql/check_affected_contents_by_id_etc.sql
+++ db-sql/change_content_context/sql/check_affected_contents_by_id_etc.sql
@@ -1,6 +1,7 @@
 --Please run with account SBR
 --ACs for KEARNSD
-select dec.dec_id, c.name, dec.date_created, dec.change_note from sbr.data_element_concepts_view dec, sbr.contexts c
+select '************** FOR KEARNSD **************' from dual;
+select dec.dec_id, c.name, dec.date_created from sbr.data_element_concepts_view dec, sbr.contexts c
 --select 'DEC_affected_for_KEARNSD = ' || count(*) from sbr.data_element_concepts_view dec, sbr.contexts c
 where dec.conte_idseq = c.conte_idseq
 and dec.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'CTEP')
@@ -8,7 +9,7 @@ and (upper(dec.created_by) = 'KEARNSD' or lower(dec.created_by) = 'KEARNSD')
 and trunc(dec.date_created) >= to_date('2001-12-01', 'YYYY-MM-DD')
 and trunc(dec.date_created) <= to_date('2013-06-25', 'YYYY-MM-DD');
 
-select de.cde_id, c.name, de.date_created, de.change_note from sbr.data_elements de, sbr.contexts c
+select de.cde_id, c.name, de.date_created from sbr.data_elements de, sbr.contexts c
 --select 'DE_affected_for_KEARNSD = ' || count(*) from sbr.data_elements de, sbr.contexts c
 where de.conte_idseq = c.conte_idseq
 and de.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'CTEP')
@@ -16,7 +17,7 @@ and (upper(de.created_by) = 'KEARNSD' or lower(de.created_by) = 'KEARNSD')
 and trunc(de.date_created) >= to_date('2001-12-01', 'YYYY-MM-DD')
 and trunc(de.date_created) <= to_date('2013-06-25', 'YYYY-MM-DD');
 
-select vd.vd_id, c.name, vd.date_created, vd.change_note from sbr.value_domains vd, sbr.contexts c
+select vd.vd_id, c.name, vd.date_created from sbr.value_domains vd, sbr.contexts c
 --select 'VD_affected_for_KEARNSD = ' || count(*) from sbr.value_domains vd, sbr.contexts c
 where vd.conte_idseq = c.conte_idseq
 and vd.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'CTEP')
@@ -25,7 +26,8 @@ and trunc(vd.date_created) >= to_date('2001-12-01', 'YYYY-MM-DD')
 and trunc(vd.date_created) <= to_date('2013-06-25', 'YYYY-MM-DD');
 
 --ACs for SMITHA
-select dec.dec_id, c.name, dec.date_created, dec.change_note from sbr.data_element_concepts_view dec, sbr.contexts c
+select '************** FOR SMITHA **************' from dual;
+select dec.dec_id, c.name, dec.date_created from sbr.data_element_concepts_view dec, sbr.contexts c
 --select 'DEC_affected_for_SMITHA = ' || count(*) from sbr.data_element_concepts_view dec, sbr.contexts c
 where dec.conte_idseq = c.conte_idseq
 and dec.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'CTEP')
@@ -33,7 +35,7 @@ and (upper(dec.created_by) = 'SMITHA' or lower(dec.created_by) = 'SMITHA')
 and trunc(dec.date_created) >= to_date('2001-12-01', 'YYYY-MM-DD')
 and trunc(dec.date_created) <= to_date('2013-06-25', 'YYYY-MM-DD');
 
-select de.cde_id, c.name, de.date_created, de.change_note from sbr.data_elements de, sbr.contexts c
+select de.cde_id, c.name, de.date_created from sbr.data_elements de, sbr.contexts c
 --select 'DE_affected_for_SMITHA = ' || count(*) from sbr.data_elements de, sbr.contexts c
 where de.conte_idseq = c.conte_idseq
 and de.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'CTEP')
@@ -41,7 +43,7 @@ and (upper(de.created_by) = 'SMITHA' or lower(de.created_by) = 'SMITHA')
 and trunc(de.date_created) >= to_date('2001-12-01', 'YYYY-MM-DD')
 and trunc(de.date_created) <= to_date('2013-06-25', 'YYYY-MM-DD');
 
-select vd.vd_id, c.name, vd.date_created, vd.change_note from sbr.value_domains vd, sbr.contexts c
+select vd.vd_id, c.name, vd.date_created from sbr.value_domains vd, sbr.contexts c
 --select 'VD_affected_for_SMITHA = ' || count(*) from sbr.value_domains vd, sbr.contexts c
 where vd.conte_idseq = c.conte_idseq
 and vd.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'CTEP')
@@ -50,7 +52,8 @@ and trunc(vd.date_created) >= to_date('2001-12-01', 'YYYY-MM-DD')
 and trunc(vd.date_created) <= to_date('2013-06-25', 'YYYY-MM-DD');
 
 --ACs for SCOTTM
-select dec.dec_id, c.name, dec.date_created, dec.change_note from sbr.data_element_concepts_view dec, sbr.contexts c
+select '************** FOR SCOTTM **************' from dual;
+select dec.dec_id, c.name, dec.date_created from sbr.data_element_concepts_view dec, sbr.contexts c
 --select 'DEC_affected_for_SCOTTM = ' || count(*) from sbr.data_element_concepts_view dec, sbr.contexts c
 where dec.conte_idseq = c.conte_idseq
 and dec.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'CTEP')
@@ -58,7 +61,7 @@ and (upper(dec.created_by) = 'SCOTTM' or lower(dec.created_by) = 'SCOTTM')
 and trunc(dec.date_created) >= to_date('2001-12-01', 'YYYY-MM-DD')
 and trunc(dec.date_created) <= to_date('2013-06-25', 'YYYY-MM-DD');
 
-select de.cde_id, c.name, de.date_created, de.change_note from sbr.data_elements de, sbr.contexts c
+select de.cde_id, c.name, de.date_created from sbr.data_elements de, sbr.contexts c
 --select 'DE_affected_for_SCOTTM = ' || count(*) from sbr.data_elements de, sbr.contexts c
 where de.conte_idseq = c.conte_idseq
 and de.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'CTEP')
@@ -66,7 +69,7 @@ and (upper(de.created_by) = 'SCOTTM' or lower(de.created_by) = 'SCOTTM')
 and trunc(de.date_created) >= to_date('2001-12-01', 'YYYY-MM-DD')
 and trunc(de.date_created) <= to_date('2013-06-25', 'YYYY-MM-DD');
 
-select vd.vd_id, c.name, vd.date_created, vd.change_note from sbr.value_domains vd, sbr.contexts c
+select vd.vd_id, c.name, vd.date_created from sbr.value_domains vd, sbr.contexts c
 --select 'VD_affected_for_SCOTTM = ' || count(*) from sbr.value_domains vd, sbr.contexts c
 where vd.conte_idseq = c.conte_idseq
 and vd.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'CTEP')
