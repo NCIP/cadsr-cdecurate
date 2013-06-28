87b5328f214cffb2319f7ac6a58f40e8e0340e7a
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-06-28 13:54:23 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@2121 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

---
+++ db-sql/change_content_context/sql/sample_output.txt.sql
@@ -0,0 +1,49 @@
+SQL*Plus: Release 10.2.0.4.0 - Production on Fri Jun 28 09:39:08 2013
+
+Copyright (c) 1982, 2007, Oracle.  All Rights Reserved.
+
+
+Connected to:
+Oracle Database 10g Enterprise Edition Release 10.2.0.5.0 - 64bit Production
+With the Partitioning, Data Mining and Real Application Testing options
+
+SQL> @change_user_ac_context-smitha.sql
+
+13 rows updated.
+
+
+15 rows updated.
+
+
+5 rows updated.
+
+
+0 rows updated.
+
+SQL> @change_user_ac_context-scottm.sql
+
+14 rows updated.
+
+
+26 rows updated.
+
+
+6 rows updated.
+
+
+0 rows updated.
+
+SQL> @change_user_ac_context-kearnsd.sql
+
+58 rows updated.
+
+
+80 rows updated.
+
+
+40 rows updated.
+
+
+0 rows updated.
+
+SQL> 
