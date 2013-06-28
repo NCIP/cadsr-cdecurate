7d112a5d086d3477ac171472fdf7caf854d38b94
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-06-28 13:54:23 +0000

    SVN-Revision: 2121

---
+++ db-sql/change_content_context/sql/change_user_ac_context-scottm.sql
@@ -0,0 +1,55 @@
+--Please run with account SBR
+update sbr.data_element_concepts dec
+set 
+dec.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'SWOG'),
+dec.change_note = dec.change_note || ' Moved content from CTEP to SWOG'
+--dec.change_note = 'Moved content from CTEP to SWOG'
+where 
+dec.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'CTEP')
+and (upper(dec.created_by) = 'scottm' or lower(dec.created_by) = 'scottm')
+and trunc(dec.date_created) >= to_date('2011-12-01', 'YYYY-MM-DD')
+and trunc(dec.date_created) <= to_date('2013-06-28', 'YYYY-MM-DD');
+
+update sbr.data_elements de
+set 
+de.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'SWOG'),
+de.change_note = de.change_note || ' Moved content from CTEP to SWOG'
+--de.change_note = 'Moved content from CTEP to SWOG'
+where 
+de.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'CTEP')
+and (upper(de.created_by) = 'scottm' or lower(de.created_by) = 'scottm')
+and trunc(de.date_created) >= to_date('2011-12-01', 'YYYY-MM-DD')
+and trunc(de.date_created) <= to_date('2013-06-28', 'YYYY-MM-DD');
+
+update sbr.value_domains vd
+set 
+vd.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'SWOG'),
+vd.change_note = vd.change_note || ' Moved content from CTEP to SWOG'
+--vd.change_note = 'Moved content from CTEP to SWOG'
+where 
+vd.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'CTEP')
+and (upper(vd.created_by) = 'scottm' or lower(vd.created_by) = 'scottm')
+and trunc(vd.date_created) >= to_date('2011-12-01', 'YYYY-MM-DD')
+and trunc(vd.date_created) <= to_date('2013-06-28', 'YYYY-MM-DD');
+
+update SBR.ADMINISTERED_COMPONENTS ac 
+set 
+ac.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'SWOG'),
+ac.change_note = ac.change_note || ' Moved content from CTEP to SWOG'
+--ac.change_note = 'Moved content from CTEP to SWOG'
+where ac.public_id in (
+select 
+--ac.date_created, ac.actl_name, 
+ac.public_id
+--, c.name, ac.created_by 
+from SBR.ADMINISTERED_COMPONENTS ac, SBR.CONTEXTS c 
+where 
+ac.conte_idseq = c.conte_idseq
+and ac.CONTE_IDSEQ = (select CONTE_IDSEQ from SBR.CONTEXTS where name = 'CTEP')
+and ac.actl_name in ('DATAELEMENT', 'VALUEDOMAIN', 'DE_CONCEPT')
+and (upper(ac.created_by) = 'scottm' or lower(ac.created_by) = 'scottm')
+and trunc(ac.date_created) >= to_date('2011-12-01', 'YYYY-MM-DD')
+and trunc(ac.date_created) <= to_date('2013-06-28', 'YYYY-MM-DD')
+);
+
+
