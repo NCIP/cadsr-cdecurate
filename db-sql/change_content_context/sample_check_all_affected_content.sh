9db9eb1432e4abc0c186c55c340f099c84def73f
James Tan <tanj3@mail.nih.gov>
2013-08-07 19:45:14 +0000

    Added some scripts to validate default context change.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cdecurate/trunk@2128 9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26

--- db-sql/change_content_context/sample_check_all_affected_content.sh
+++ db-sql/change_content_context/sample_check_all_affected_content.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #************* DATABASE  SETTINS *************
 export user=SBR
-export pwd=jjuser
+export pwd=
 export SQLPLUS_PATH=~/Dropbox/nih/instantclient_10_2/sqlplus
 #export SQLPLUS_PATH=sqlplus
 export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=137.187.181.4)(port=1551)))(connect_data=(SID=DSRDEV)))"
@@ -9,9 +9,7 @@ export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=137.187.18
  
 #************* APPLICATION SETTINS *************
 #NONE
- 
-$SQLPLUS_PATH $user/$pwd$HOST @check_user_ac_context.sql 'CTEP' 'SWOG' '2001-12-01' '2013-06-19' 'KEARNSD' > /tmp/check_ac_counts.txt 2>&1
- 
-$SQLPLUS_PATH $user/$pwd$HOST @check_user_ac_context.sql 'CTEP' 'SWOG' '2001-12-01' '2013-06-19' 'SMITHA' >> /tmp/check_ac_counts.txt 2>&1
- 
-$SQLPLUS_PATH $user/$pwd$HOST @check_user_ac_context.sql 'CTEP' 'SWOG' '2001-12-01' '2013-06-19' 'SCOTTM' >> /tmp/check_ac_counts.txt 2>&1
+
+$SQLPLUS_PATH $user/$pwd$HOST @check_all_user_ac_context.sql 'NCIP' 'NCIP' '2001-01-01' '2013-08-07' '%%' > ./check_all_ac_counts_NCIP.txt 2>&1
+
+$SQLPLUS_PATH $user/$pwd$HOST @check_all_user_ac_context.sql 'caBIG' 'caBIG' '2001-01-01' '2013-08-07' '%%' > ./check_all_ac_counts_caBIG.txt 2>&1
