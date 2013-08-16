b7f0e6cc8b187913cf5263c7ad3cff4ffbc3bdcc
tanj3 <tanj3@9773d6d1-8b9d-42ed-ac85-0f7fbd9fed26>
2013-08-16 20:43:44 +0000

    SVN-Revision: 2141

--- src/gov/nih/nci/cadsr/common/Constants.java
+++ src/gov/nih/nci/cadsr/common/Constants.java
@@ -4,4 +4,9 @@ public class Constants {
 	public static final String DEFAULT_CONTEXT = "NCIP";
 	public static final String DEFAULT_MANDATORY_ATTRIBUTE_TEXT = "This field is Mandatory. \n";
 	public static final String DEC_CDR_NAME = "dec_cdr_name";
+	
+	public static final String USER_SELECTED_ALTERNATE_DEF_COMP1 = "objectQualifierMap_UserALtDef";
+	public static final String USER_SELECTED_ALTERNATE_DEF_COMP2 = "object_UserALtDef";
+	public static final String USER_SELECTED_ALTERNATE_DEF_COMP3 = "propQualifierMap_UserALtDef";
+	public static final String USER_SELECTED_ALTERNATE_DEF_COMP4 = "prop_UserALtDef";
 }
