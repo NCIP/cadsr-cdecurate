b36d5e5e6767aed416f7c9f8a36167907a64e8ac
ghguy <jamestanms@hotmail.com>
2013-09-18 13:40:38 -0400

    GF32153 changed the message based the feedback from the sponsor.

--- src/gov/nih/nci/cadsr/cdecurate/util/ErrorHelper.java
+++ src/gov/nih/nci/cadsr/cdecurate/util/ErrorHelper.java
@@ -1,14 +1,15 @@
 package gov.nih.nci.cadsr.cdecurate.util;
 
+import gov.nih.nci.cadsr.common.Constants;
 import gov.nih.nci.cadsr.common.PropertyHelper;
 
 public class ErrorHelper {
 
 	//GF32153
 	public static final String getLoginMessage() {
-		String message = "<h2>We're Sorry</h2>" +
-				"<div style=\"font-size: 12pt;font-weight:bold;\">We understand the inconvenience this has caused, and we are very sorry for this!" +
-				"<p style=\"font-size: 12pt;font-weight:bold;\">You could try again or visit <a href='" + PropertyHelper.getPCSURL() + "'>" + PropertyHelper.getPCSName() + " (" + PropertyHelper.getPCSURL() + ")</a> to reset your password.</div>";
+		String message = //"<h2>We're Sorry</h2>" +
+				"<div style=\"font-size: 12pt;font-weight:bold;\">" + Constants.ERR_LOGON_ISSUE + " You could try up to 6 attempts before being locked out, or visit " + PropertyHelper.getPCSName() + " at <a href='" + PropertyHelper.getPCSURL() + "'>" + PropertyHelper.getPCSURL() +"</a> to reset your password. " +
+				"</div>";
 				
 		return message;
 	}
