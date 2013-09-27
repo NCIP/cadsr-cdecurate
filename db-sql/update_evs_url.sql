fabcf7f80c36faae18afcf19399033efeb41b174
jamestan <jamestanms@hotmail.com>
2013-09-27 13:15:23 -0500

    Comment out unused endpoint for lexEVS 6.0.3.

--- db-sql/update_evs_url.sql
+++ db-sql/update_evs_url.sql
@@ -5,9 +5,9 @@
   See http://ncip.github.com/cadsr-cdecurate/LICENSE.txt for details.
 L*/
 
-update sbrext.tool_options_view_ext set value = 'http://lexevsapi60.nci.nih.gov/lexevsapi60'
-where Tool_name = 'CURATION' and Property = 'EVS.URL'
-/
+--update sbrext.tool_options_view_ext set value = 'http://lexevsapi60.nci.nih.gov/lexevsapi60'
+--where Tool_name = 'CURATION' and Property = 'EVS.URL'
+--/
 update sbrext.tool_options_view_ext set value = 'http://lexevsapi60.nci.nih.gov/lexevsapi60'
 where Tool_name = 'EVSAPI' and Property = 'URL'
 /
