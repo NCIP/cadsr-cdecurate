8cfcbd1b7daec12cac74ae951e349317231f7366
jamestan <jamestanms@hotmail.com>
2013-11-12 15:03:43 -0600

    https://gforge.nci.nih.gov/tracker/index.php?func=detail&aid=33051&group_id=60&atid=323

--- db-sql/create_new_contexts/create_context_PROD.sh
+++ db-sql/create_new_contexts/create_context_PROD.sh
@@ -10,4 +10,4 @@ export SQLPLUS_PATH=sqlplus
 export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=ncidb-dsr-p.nci.nih.gov)(port=1551)))(connect_data=(SID=DSRPROD)))"
 
 echo $SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'COG' "Children's Oncology Group"
-$SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'COG' "Children's Oncology Group"
+$SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'COG' "Children''s Oncology Group"
