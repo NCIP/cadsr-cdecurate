aa9d834bc8dd62f6177b6a97c614ad00a797aaaf
jamestan <jamestanms@hotmail.com>
2013-12-31 11:09:10 -0500

    GF33087

---
+++ db-sql/change_content_context/support/GF33097/change_context_PROD_muliple_users.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+#Please run with user SBR
+#************* DATABASE SETTINS *************
+#------------- Please change according to your environment before executing the script -------------
+echo input parameters in the order specified:
+echo "old_context to new_context from_date(format YYYY-MM-DD) end_date (format YYYY-MM-DD) curator_id"
+export user=SBR
+export pwd=xxx
+#export SQLPLUS_PATH=~/Dropbox/nih/instantclient_10_2/sqlplus
+export SQLPLUS_PATH=sqlplus
+#export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=137.187.181.4)(port=1551)))(connect_data=(SID=DSRDEV)))"
+export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=ncidb-dsr-p.nci.nih.gov)(port=1551)))(connect_data=(SID=DSRPROD)))"
+
+#************* APPLICATION SETTINS *************
+#------------- Please change according to the request before executing the script -------------
+export FROM_CONTEXT=CTEP
+export TO_CONTEXT=COG
+
+export DATE_TIME=$(date)
+export CHANGE_NOTE="'Changed from $FROM_CONTEXT to $TO_CONTEXT for $CREATOR by script $DATE_TIME'"
+#echo $CHANGE_NOTE
+#exit
+
+echo 'Purging old backup ...'
+$SQLPLUS_PATH $user/$pwd$HOST @change_user_ac_context_cleanup.sql
+echo 'Backup purged! About to execute the real stuff ...'
+
+export FROM_DATE=2010-02-04
+export TO_DATE=2012-03-16
+export CREATOR=HARTLEYG
+$SQLPLUS_PATH $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR $CHANGE_NOTE
+$SQLPLUS_PATH $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
+export FROM_DATE=2010-03-19
+export TO_DATE=2013-10-22
+export CREATOR=WATSONB
+$SQLPLUS_PATH $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR $CHANGE_NOTE
+$SQLPLUS_PATH $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
+export CREATOR=GEORGIEB
+$SQLPLUS_PATH $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR $CHANGE_NOTE
+$SQLPLUS_PATH $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
+export CREATOR=MATTHEWK
+$SQLPLUS_PATH $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR $CHANGE_NOTE
+$SQLPLUS_PATH $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
+export CREATOR=SIDDIQIF
+$SQLPLUS_PATH $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR $CHANGE_NOTE
+$SQLPLUS_PATH $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
+export CREATOR=MARTINC
+$SQLPLUS_PATH $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR $CHANGE_NOTE
+$SQLPLUS_PATH  $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
+export CREATOR=STEWARDN
+$SQLPLUS_PATH $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR $CHANGE_NOTE
+$SQLPLUS_PATH $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
+export CREATOR=BEELEST
+$SQLPLUS_PATH $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR $CHANGE_NOTE
+$SQLPLUS_PATH $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
+export CREATOR=HASENAUB
+$SQLPLUS_PATH $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR $CHANGE_NOTE
+$SQLPLUS_PATH $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
+export CREATOR=CORRALC
+$SQLPLUS_PATH $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR $CHANGE_NOTE
+$SQLPLUS_PATH $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
+export CREATOR=TSANE
+$SQLPLUS_PATH $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR $CHANGE_NOTE
+$SQLPLUS_PATH $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
+export CREATOR=SUBRAMAS
+$SQLPLUS_PATH $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR $CHANGE_NOTE
+$SQLPLUS_PATH $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
+export CREATOR=JONGS
+$SQLPLUS_PATHs $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR $CHANGE_NOTE
+$SQLPLUS_PATHs $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
+export CREATOR=WONGW
+$SQLPLUS_PATH $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR $CHANGE_NOTE
+$SQLPLUS_PATH $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
+export CREATOR=REEDK
+$SQLPLUS_PATH $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR $CHANGE_NOTE
+$SQLPLUS_PATH $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
+export CREATOR=DONOVANN
+$SQLPLUS_PATH $user/$pwd$HOST @change_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR $CHANGE_NOTE
+$SQLPLUS_PATH $user/$pwd$HOST @check_user_ac_context.sql $FROM_CONTEXT $TO_CONTEXT $FROM_DATE $TO_DATE $CREATOR
---
+++ db-sql/change_content_context/support/GF33097/change_user_ac_context_cleanup.sql
@@ -0,0 +1,17 @@
+/*L
+  Copyright ScenPro Inc, SAIC-F
+
+  Distributed under the OSI-approved BSD 3-Clause License.
+  See http://ncip.github.com/cadsr-cdecurate/LICENSE.txt for details.
+L*/
+
+--Please run with account SBR
+DROP TABLE SBR.AC_BACKUP1;
+
+DROP TABLE SBR.DEC_BACKUP1;
+
+DROP TABLE SBR.DE_BACKUP1;
+
+DROP TABLE SBR.VD_BACKUP1;
+
+exit
