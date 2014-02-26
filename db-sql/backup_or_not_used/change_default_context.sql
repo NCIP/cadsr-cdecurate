bf5138a0db585a1806ceb2e8abb600151d9ef48f
tanj3 <tanj3@NCI-01870532-L.nci.nih.gov>
2014-02-26 12:19:03 -0500

    NCI changed to NCIP.

--- db-sql/backup_or_not_used/change_default_context.sql
+++ db-sql/backup_or_not_used/change_default_context.sql
@@ -6,7 +6,7 @@
 L*/
 
 --Run with SBREXT user
-update sbr.contexts_view  set name='NCI' where conte_idseq=(select tv.value from sbrext.tool_options_view_ext tv , sbr.contexts_view  cv where tv.tool_name = 'caDSR' and tv.property = 'DEFAULT_CONTEXT' and tv.value = cv.conte_idseq)
+update sbr.contexts_view  set name='NCIP' where conte_idseq=(select tv.value from sbrext.tool_options_view_ext tv , sbr.contexts_view  cv where tv.tool_name = 'caDSR' and tv.property = 'DEFAULT_CONTEXT' and tv.value = cv.conte_idseq)
 /
 commit
 /
