6f07fa3b06d63a1107da68eb9c9bdf80ef34d050
unknown <tanj3@NCI-01870532-L.nci.nih.gov>
2014-04-02 11:36:01 -0400

    GF33275

--- db-sql/create_new_contexts/create_context.sh
+++ db-sql/create_new_contexts/create_context.sh
@@ -9,7 +9,6 @@ export SQLPLUS_PATH=sqlplus
 export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=137.187.181.4)(port=1551)))(connect_data=(SID=DSRDEV)))"
 
 echo 'Creating context ...'
-echo 
-$SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'ABTC' 'Adult Brain Tumor Consortium'
-$SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'ABTC' 'Adult Brain Tumor Consortium'
+echo $SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'SDC Pilot Project' 'SDC Pilot Project' 
+$SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'SDC Pilot Project' 'SDC Pilot Project'
 echo 'Done'
--- db-sql/create_new_contexts/create_context_PROD.sh
+++ db-sql/create_new_contexts/create_context_PROD.sh
@@ -10,9 +10,6 @@ export SQLPLUS_PATH=sqlplus
 export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=ncidb-dsr-p.nci.nih.gov)(port=1551)))(connect_data=(SID=DSRPROD)))"
 
 echo 'Creating context ...'
-echo
-$SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'ABTC' 'Adult Brain Tumor
-Consortium'
-$SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'ABTC' 'Adult Brain Tumor
-Consortium'
+echo $SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'SDC Pilot Project' 'SDC Pilot Project'
+$SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'SDC Pilot Project' 'SDC Pilot Project'
 echo 'Done'
