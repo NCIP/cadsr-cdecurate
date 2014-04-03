164fe62d3b63400ad95b08c0c2a77db97911818a
tanj3 <tanj3@NCI-01870532-L.nci.nih.gov>
2014-04-03 14:50:18 -0400

    GF33275 Create new Context 'SDC Pilot Project'

---
+++ db-sql/create_new_contexts/create_context_STAGE.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#Please run with user SBR
+#Wiki https://wiki.nci.nih.gov/display/caDSRproj/Moving+the+Context+of+Content
+#----------------------------------------------------------------------------------------------------------------------------------
+#	DATABASE SETTINGS
+
+export user=SBR
+export pwd=replace_me
+export SQLPLUS_PATH=sqlplus
+export HOST=@"(description=(address_list=(address=(protocol=TCP)(host=ncidb-dsr-s.nci.nih.gov)(port=1551)))(connect_data=(SID=DSRSTG)))"
+
+
+echo 'Creating context ...'
+echo $SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'SDC Pilot Project' 'SDC Pilot Project'
+$SQLPLUS_PATH $user/$pwd$HOST @run_create_context.sql 'SDC Pilot Project' 'SDC Pilot Project'
+echo 'Done'
