acce9d9db095a584a56ea4ec8f6cc7d3813a9f50
unknown <tanj3@NCI-01870532-L.nci.nih.gov>
2014-05-02 12:32:09 -0400

    Updated unit tests

---
+++ test/js/helpers/mock-gf7680.js
@@ -0,0 +1,63 @@
+"use strict";
+
+/**
+ * Define a CommonJS-style module to isolate the mocking codes in a module
+ */
+define(function (require, exports, module) {
+    //var dep1 = require('app/foo');
+    exports.view = function (pvd, imgdhide, imgddisp, action, pvNo, vdwfstatus, vdusedinform, pvusedinform, fmwfstatus) {
+        /**
+         * These piece of codes should have been wrapped in function but it didn't. Thus this mock is created to make it testable.
+         */
+        var status = '';
+
+        if(fmwfstatus !== 'null') { //only for existing VD
+            if ((vdwfstatus === 'RELEASED' && vdusedinform === 'true' && pvusedinform === 'true') /* cond 2 and 3 */ ||
+                (vdwfstatus !== 'RELEASED' && vdusedinform === 'true' && pvusedinform === 'true' && fmwfstatus === 'RELEASED' /* cond 5 */)
+                ) {
+                //#1 disablement
+                dojo.query("img.PVAction").forEach(function (node, index) {
+                    try {
+                        var altText = node.getAttribute('alt');
+                        //window.console && console.log('PVAction altText [' + altText + ']');
+                        if (altText === 'Remove') {
+                            dojo.style(node, 'display', 'none');
+                            window.console && console.log('PVAction remove disabled');
+                            status = 'PVAction remove disabled';
+                        }
+                    } catch (e) {
+                        window.console && console.log('Error: Not able to disable PVAction remove, ' + e);
+                    }
+                });
+            }
+            if ((vdwfstatus === 'RELEASED' && vdusedinform === 'true' && pvusedinform === 'true') /* cond 2 and 3 */ ||
+                (vdwfstatus === 'RELEASED' && vdusedinform === 'true' && pvusedinform === 'false') /* cond 4 */ ||
+                (vdwfstatus !== 'RELEASED' && vdusedinform === 'true' && pvusedinform === 'true' && fmwfstatus === 'RELEASED' /* cond 5 */)
+                ) {
+                //#2 disablement
+                dojo.query('[id^="txtpv"]').forEach(function (node, index, arr) { //GF7680 "txtpvonly" has to be the same as gov.nih.nci.cadsr.common.PV_NAME
+                    try {
+                        dojo.attr(node, "readonly", true);
+                        window.console && console.log('PV/VM input disabled');
+                        status = 'PV/VM input disabled';
+                    } catch (e) {
+                        window.console && console.log('Error: Not able to disable PV/VM input, ' + e);
+                    }
+                });
+            }
+            //#4 please keep the following for future requirements changes, if any
+            /*
+             dojo.query("img.ConceptAction").forEach(function(node, index){
+             var altText = node.getAttribute('alt');
+             //window.console && console.log('ConceptAction altText [' + altText + ']');
+             if(altText === 'Remove') {
+             dojo.style(node, 'display', 'none');
+             window.console && console.log('ConceptAction remove disabled');
+             }
+             });
+             */
+
+            return status;
+        }
+    };
+});
--- test/js/units-qun.js
+++ test/js/units-qun.js
@@ -6,7 +6,7 @@ test( "GF7680 Test 1", function() {
     view('pv0View', 'pv0ImgClose', 'pv0ImgOpen', 'view', 'pv0', 'RELEASED','true','true','RELEASED');
     var elem = document.getElementsByName("textarea");
     //equal(elem.readOnly, true);
-    equal(elem.readOnly, undefined);
+    equal(elem.readOnly, undefined, "Element is undefined");
 });
 
 test( "GF7680 Test 2", function() {
@@ -18,7 +18,21 @@ test( "GF7680 Test 2", function() {
 //    var elem = $("img.PVAction");
 //    equals(elem.style.display, "none");
     var elem = document.getElementsByName("textarea");
-    //equal(elem.readOnly, true);
-    equal(elem.readOnly, undefined);
+    notEqual(elem.readOnly, true, "Readonly is not found");
+    //equal(elem.readOnly, undefined);
+});
 
+asyncTest( "GF7680 Test 3", function() {
+    var ret1, ret2;
+    QUnit.config.autostart = false;
+    QUnit.log(function( details ) {
+        console.log( "QUnit Log: ", details.result, details.message );
+    });
+    curl(['helpers/mock-gf7680'], function (mock, ret1, ret2) {
+        QUnit.start();
+        ret1 = mock.view('pv0View', 'pv0ImgClose', 'pv0ImgOpen', 'view', 'pv0', 'RELEASED','true','true','RELEASED');
+        equal(ret1, 'PVAction remove disabled', "PV delete is disabled");
+        ret2 = mock.view('pv0View', 'pv0ImgClose', 'pv0ImgOpen', 'view', 'pv0', 'NOT RELEASED','true','true','');
+        notEqual(ret2, 'PVAction remove disabled', "PV delete is enabled");
+    });
 });
