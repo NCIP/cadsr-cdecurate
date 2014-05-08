c66b7bacc259d08a273cfa7eca5f29967d939301
unknown <jamestanms@hotmail.com>
2014-05-08 19:18:37 -0400

    Updated unit tests

--- test/js/units-jas-1.3.1.js
+++ test/js/units-jas-1.3.1.js
@@ -33,36 +33,47 @@ describe('GF7680', function() {
     /** define a test specs */
     it('PV delete should be disabled', function () {
         callMock('mock-gf7680', function(mock) {
+            //jasmine.log(mock);
             ret = mock.view('pv0View', 'pv0ImgClose', 'pv0ImgOpen', 'view', 'pv0', 'RELEASED', 'true', 'true', 'RELEASED');
+            //jasmine.log(ret);
             expect(ret).toContain('PVAction remove disabled');
             ret = mock.view('pv0View', 'pv0ImgClose', 'pv0ImgOpen', 'view', 'pv0', 'NOT RELEASED', 'true', 'true', 'RELEASED');
+            //jasmine.log(ret);
             expect(ret).toContain('PVAction remove disabled');
         });
     });
 
     it('PV delete should be enabled', function () {
         callMock('mock-gf7680', function(mock) {
+            //jasmine.log(mock);
             ret = mock.view('pv0View', 'pv0ImgClose', 'pv0ImgOpen', 'view', 'pv0', 'NOT RELEASED', 'true', 'true', '');
+            //jasmine.log(ret);
             expect(ret).not.toContain('PVAction remove disabled');
         });
     });
 
     it('PV/VM input should be disabled', function () {
         callMock('mock-gf7680', function(mock) {
+            //jasmine.log(mock);
             var expectedStat = 'PV/VM input disabled';
             ret = mock.view('pv0View', 'pv0ImgClose', 'pv0ImgOpen', 'view', 'pv0', 'RELEASED', 'true', 'true', 'WHATEVER');
+            //jasmine.log(ret);
             expect(ret).toContain(expectedStat);
             ret = mock.view('pv0View', 'pv0ImgClose', 'pv0ImgOpen', 'view', 'pv0', 'RELEASED', 'true', 'false', 'WHATEVER');
+            //jasmine.log(ret);
             expect(ret).toContain(expectedStat);
             ret = mock.view('pv0View', 'pv0ImgClose', 'pv0ImgOpen', 'view', 'pv0', 'NOT RELEASED', 'true', 'true', 'RELEASED');
+            jasmine.log(ret);
             expect(ret).toContain(expectedStat);
         });
     });
 
     it('PV/VM input should be enabled', function () {
         callMock('mock-gf7680', function(mock) {
+            //jasmine.log(mock);
             var expectedStat = 'PV/VM input disabled';
             ret = mock.view('pv0View', 'pv0ImgClose', 'pv0ImgOpen', 'view', 'pv0', 'NOT RELEASED', 'false', 'true', 'WHATEVER');
+            //jasmine.log(ret);
             expect(ret).not.toContain(expectedStat);
         });
     });
@@ -71,25 +82,16 @@ describe('GF7680', function() {
 var stats = [];
 describe('GF32723', function() {
     var ret;
-    var spec = [];
+    var spec = ['Alternate Name Specs'];
     var specm = [];
-    var callDone;
 
-    beforeEach(function (done) {
-        spec.push('Alternate Name Specs');
+    beforeEach(function () {
         specm.push('Vocabulary picked should be NCIt');
         specm.push('Altername name should not be created');
-        done();
-    });
-
-    afterEach(function(done){
-        expect(stats).toEqual(['NCI Thesaurus', 'valid_submitted']);
-        done();
     });
 
     /** define a test specs */
-    it(spec[0], function (done) {
-        callDone = done;
+    it(spec[0], function () {
         var ret;
         //TODO: for PhantomJS to avoid timeout
 //        requirejs(['phantom'], function(phantom) {
@@ -101,32 +103,40 @@ describe('GF32723', function() {
 //        });
         //TODO: for PhantomJS to avoid timeout
 
-//        function callDone() {
-//            done();
-//        }
+        runs(function () {
+            try {
+                callMock('mock-gf32723', function (mock) {
+                    //jasmine.log(mock);
+                    if (typeof document !== 'undefined') {
+                        mock.pickVocab(1, "NCI Thesaurus");
+                        ret = mock.doVocabChange();
+                        stats.push(ret);
+                        expect(function () {
+                            mock.SubmitValidate()
+                        }).not.toThrow(); //"No raised error during submission"
+                        ret = mock.SubmitValidate('validate');  //"Alternate name successfully submitted"
+                        stats.push(ret);
+                    } else {
+                        jasmine.log('Skipped!');
+                        expect(true).toBe(true);
+                    }
+                });
+            } catch (e) {
+                console.log('units-jas-1.3.1.js GF32723 error: ' + e);
+            }
+            //expect(false).toBe(true);
+        });
 
-        try {
-            callMock('mock-gf32723', function (mock) {
-                if (typeof document !== 'undefined') {
-                    mock.pickVocab(1, "NCI Thesaurus");
-                    ret = mock.doVocabChange();
-                    stats.push(ret);
-                    expect(function () {
-                        mock.SubmitValidate()
-                    }).not.toThrow(); //"No raised error during submission"
-                    ret = mock.SubmitValidate('validate');  //"Alternate name successfully submitted"
-                    stats.push(ret);
-                    callDone();
-//                    done();
-                } else {
-//                    jasmine.log('Skipped!');
-                    expect(true).toBe(true);
-                }
-            });
-        } catch (e) {
-            console.log('units-jas-1.3.1.js GF32723 error: ' + e);
-        }
+        waitsFor(function() {
+            var flag;
+            if(stats.length === 2) {
+                expect(stats).toEqual(['NCI Thesaurus', 'valid_submitted']);
 
+                console.log("Async spec done!");
+                flag = true;
+            }
+            return flag;
+        }, spec[0], 1500);
     });
 
 })
