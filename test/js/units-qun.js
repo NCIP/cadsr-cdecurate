645b88268698b8d2bfe788b64451ca15e437c240
unknown <jamestanms@hotmail.com>
2014-05-13 19:54:40 -0400

    Updated unit tests

--- test/js/units-qun.js
+++ test/js/units-qun.js
@@ -56,8 +56,11 @@ test("GF7680 Test 1", function() {
         QUnit.config.autostart = false;
         //ok( 1 == "1", "qunit is ready!" );  //just a sanity check to make sure that QUnit works! ;)
 
+        console.time("GF7680 perf 1");
         arrValidate[0] = arrValidate[2] = 'qunitTest';   //remove "view" and avoid "Please save the Permissible Value
         view('pv0View', 'pv0ImgClose', 'pv0ImgOpen', 'view', 'pv0', 'RELEASED', 'true', 'true', 'RELEASED');
+        console.log("V8 [12.00ms] IE11 [9.58ms] FF [13.33ms]");
+        console.timeEnd("GF7680 perf 1");
         var elem = document.getElementsByName("textarea");
         //equal(elem.readOnly, true);
         equal(elem.readOnly, undefined, "Element is undefined (textarea is left as is)");
@@ -71,8 +74,11 @@ test("GF7680 Test 2", function() {
         QUnit.config.autostart = false;
         //ok( 1 == "1", "qunit is ready!" );  //just a sanity check to make sure that QUnit works! ;)
 
+        console.time("GF7680 perf 2");
         arrValidate[0] = arrValidate[2] = 'qunitTest';   //remove "view" and avoid "Please save the Permissible Value
         view('pv0View', 'pv0ImgClose', 'pv0ImgOpen', 'view', 'pv0', 'RELEASED','true','true','NOT RELEASED');
+        console.log("V8 [8.00ms] IE11 [3.54ms] FF [4.84ms]");
+        console.timeEnd("GF7680 perf 2");
 //        var elem = $("img.PVAction");
 //        equals(elem.style.display, "none");
         var elem = document.getElementById("txtpvonly0");
