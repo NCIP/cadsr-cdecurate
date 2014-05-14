9f06448c2e089c4dba2f56e05e0395235582a47f
unknown <tanj3@NCI-01870532-L.nci.nih.gov>
2014-05-14 13:20:38 -0400

    https://tracker.nci.nih.gov/browse/CURATNTOOL-665#comment-381040; updated test units

--- test/gov/nih/nci/cadsr/cdecurate/test/legacy/GF30798_OCQ_OC_PropQ_Prop_Selected.java
+++ test/gov/nih/nci/cadsr/cdecurate/test/legacy/GF30798_OCQ_OC_PropQ_Prop_Selected.java
@@ -1,10 +1,9 @@
-package gov.nih.nci.cadsr.cdecurate.test.gf;
+package gov.nih.nci.cadsr.cdecurate.test.legacy;
 
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -13,13 +12,12 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.WebDriverBackedSelenium;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -30,105 +28,64 @@ import com.thoughtworks.selenium.Selenium;
   * Setup:
   * sudo chmod 755 /Users/ag/demo/cdecurate/lib/webdriver/chromedriver
   */
-public class GF30798 {
-//	public static final String baseUrl = "https://cdecurate-dev.nci.nih.gov";
-	public static final String baseUrl = "http://localhost:8080";
-	public static final String p = "Te$t1239";
-	
-	public static enum BROWSER_TYPE {
-		CHROME, FIREFOX, IE, SAFARI, OPERA
-	}
+public class GF30798_OCQ_OC_PropQ_Prop_Selected {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
 
-//	public static final BROWSER_TYPE BTYPE = BROWSER_TYPE.CHROME;
-	public static final BROWSER_TYPE BTYPE = BROWSER_TYPE.FIREFOX;
-
+//begin jt
 	private static ChromeDriverService service;
-	private static Selenium selenium;
-	private WebDriver driver;
 
-	@BeforeClass
-	public static void createAndStartService() throws IOException {
-		if (BTYPE == BROWSER_TYPE.CHROME) {
-			service = new ChromeDriverService.Builder()
-					.usingDriverExecutable(
-							new File("lib/webdriver/chromedriver"))
-					.usingAnyFreePort().build();
-			service.start();
-		}
-	}
+	  @BeforeClass
+	  public static void createAndStartService() throws IOException {
+	    service = new ChromeDriverService.Builder()
+	        .usingDriverExecutable(new File("lib/webdriver/chromedriver"))
+	        .usingAnyFreePort()
+	        .build();
+	    service.start();
+	  }
 
-	@AfterClass
-	public static void createAndStopService() {
-//		if (BTYPE == BROWSER_TYPE.CHROME) {
-//			service.stop();
-//		}
-	}
+	  @AfterClass
+	  public static void createAndStopService() {
+	    service.stop();
+	  }
 
-	@Before
-	public void createDriver() {
-		if (BTYPE == BROWSER_TYPE.CHROME) {
-			driver = new RemoteWebDriver(service.getUrl(),
-					DesiredCapabilities.chrome());
-		} else //if (BTYPE == BROWSER_TYPE.FIREFOX) 
-		{
-			FirefoxProfile fp = new FirefoxProfile();
-			fp.setPreference("webdriver.load.strategy", "unstable"); // As of
-																		// 2.19.
-																		// from
-																		// 2.9
-																		// -
-																		// 2.18
-																		// use
-																		// 'fast'
-			// fp.setPreference("webdriver_enable_native_events", true);
-			// fp.setEnableNativeEvents(true);
-			driver = new FirefoxDriver(fp);
-		}
-//		selenium = new WebDriverBackedSelenium(driver,
-//				"http://localhost:8888");
-//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
-		LogoutLogin();
-	}
+	  @Before
+	  public void createDriver() {
+		    driver = new RemoteWebDriver(service.getUrl(),
+			        DesiredCapabilities.chrome());
+//	    baseUrl = "https://cdecurate-dev.nci.nih.gov/cdecurate/NCICurationServlet?reqType=homePage";
+//	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    
+	  }
 
-	@After
-	public void quitDriver() {
-//		driver.quit();
-	}
-
-	//@Test
-	public void checkGoogleSearch() {
-		driver.get("http://www.google.com");
-		WebElement searchBox = driver.findElement(By.name("q"));
-		searchBox.sendKeys("webdriver");
-		searchBox.clear();
-		assertEquals("Google", driver.getTitle());
-	}
-
-	  public void LogoutLogin() {
-	    driver.get(baseUrl + "/cdecurate");
-//	    driver.findElement(By.linkText("Logout")).click();
-	    driver.findElement(By.linkText("Login")).click();
-		driver.findElement(By.name("Username")).sendKeys("tanj");
-		driver.findElement(By.name("Password")).sendKeys(p+Keys.ENTER);	    
-	    driver.findElement(By.name("login")).click();
+	  @After
+	  public void quitDriver() {
+	    driver.quit();
 	  }
 	  
-  public void waitForPopUp(long miliseconds) throws InterruptedException {
-    for (String handle : driver.getWindowHandles()) {
-        driver.switchTo().window(handle);
-	}
-    Thread.sleep(miliseconds);
-  }
-  
-  @Test
+//	  @Test
+	  public void testGoogleSearch() {
+	    driver.get("http://www.google.com");
+	    WebElement searchBox = driver.findElement(By.name("q"));
+	    searchBox.sendKeys("webdriver");
+	    searchBox.clear();
+	    assertEquals("Google", driver.getTitle());
+	  }
+//end jt	  
+
+
+
+//  @Test
   public void testGF30798OCQOCPropQPropSelected() throws Exception {
     driver.get(baseUrl + "/cdecurate/NCICurationServlet?reqType=login");
     driver.findElement(By.xpath("//td[@onclick=\"menuShow(this, event, 'no');\"]")).click();
     driver.findElement(By.xpath("(//dt[@id=''])[15]")).click();
     driver.findElement(By.linkText("Search")).click();
-    waitForPopUp(3000);
-    driver.findElement(By.name("keyword")).sendKeys("minor"+Keys.ENTER);
+    // ERROR: Caught exception [ERROR: Unsupported command [waitForPopUp | BlockSearch | 30000]]
+    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=BlockSearch | ]]
+    driver.findElement(By.name("keyword")).sendKeys("minor");
     driver.findElement(By.name("CK0")).click();
     driver.findElement(By.name("editSelectedBtn")).click();
     // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | null | ]]
